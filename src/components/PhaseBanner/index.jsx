40d9a366597432371f020a5fcf4b2af1eeef07e5
Oktawia Kata <oktawia.kata@thoughtworks.com>
2020-01-07 13:29:18 +0000

    PRMT-788 [DS/OK] Adding a gp2gp prefix to custom components and styling

--- src/components/PhaseBanner/index.jsx
+++ src/components/PhaseBanner/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.scss";
 
 const PhaseBanner = ({ tag, children }) => (
-  <div className="nhsuk-phase-banner">
+  <div className="gp2gp-phase-banner">
     <p className="nhsuk-body-s nhsuk-u-margin-bottom-3">
       <strong className="nhsuk-u-margin-right-3">{tag}</strong>
       {children}
--- src/components/PracticeDetails/index.jsx
+++ src/components/PracticeDetails/index.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import "./index.scss";
 
 const PracticeDetails = ({ name, ODSCode, address }) => (
-  <div className="nhsuk-practice-details">
+  <div className="gp2gp-practice-details">
     <h1 className="nhsuk-heading-l">
       {name} | {ODSCode}
     </h1>
     <address>
       {address.lines.map((line, i) => (
         <span
-          className="nhsuk-practice-details__address-line"
+          className="gp2gp-practice-details__address-line"
           key={`address-${i}`}
         >
           {line}
         </span>
       ))}
-      <span className="nhsuk-practice-details__address-line">
+      <span className="gp2gp-practice-details__address-line">
         {address.town}
       </span>
-      <span className="nhsuk-practice-details__address-line">
+      <span className="gp2gp-practice-details__address-line">
         {address.postCode}
       </span>
     </address>
