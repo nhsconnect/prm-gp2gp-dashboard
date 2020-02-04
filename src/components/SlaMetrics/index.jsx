a0966ea088a4a0eb4d54c2165949b114155997ed
Dorota-TW <dorota.sobkow@thoughtworks.com>
2020-02-04 17:04:43 +0000

    PRMT-791 [OK/DS] Display SLA metrics on practice page

---
+++ src/components/SlaMetrics/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.scss";
+
+const SlaMetric = ({ label, value }) => (
+  <div className="gp2gp-sla-metrics__item"><span className="nhsuk-body-s nhsuk-u-margin-bottom-0">{label}</span>
+    <span className="nhsuk-body-s">{value}</span></div>
+)
+
+const SlaMetrics = ({ title, subtitle, metrics }) => (
+  <div className="gp2gp-sla-metrics">
+    <h3 className="nhsuk-body-s nhsuk-u-margin-bottom-0">{title}</h3>
+    <p className="nhsuk-body-s nhsuk-u-margin-bottom-0">{subtitle}</p>
+    <div className="gp2gp-sla-metrics__items-wrapper">
+      {metrics.map((metric, i) => (<SlaMetric key={`label-${i}`} label={metric.label} value={metric.value} />))}
+    </div>
+  </div>
+);
+
+export default SlaMetrics;
--- src/templates/practice.js
+++ src/templates/practice.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useStaticQuery, graphql } from "gatsby";
 import PracticeDetails from "../components/PracticeDetails/index";
 import { getPracticeDetails } from "../library/api/ODSPortal";
 import { convertMonthNumberToText } from "../library/common/index";
+import SlaMetrics from "../components/SlaMetrics";
 
 const Practice = ({ pageContext }) => {
   const [practiceDetails, setPracticeDetails] = useState({
@@ -15,17 +17,51 @@ const Practice = ({ pageContext }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const { name, ODSCode, address } = practiceDetails;
-  const month = convertMonthNumberToText(pageContext.month);
-  const year = pageContext.year;
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allFile(filter: { name: { eq: "sla" } }) {
+          edges {
+            node {
+              childContentJson {
+                title
+                subtitle
+                within3Days
+                within8Days
+                beyond8Days
+              }
+            }
+          }
+        }
+      }
+    `
+  ).allFile.edges[0].node.childContentJson;
 
+  const { name, ODSCode, address } = practiceDetails;
+  const { month, year, metrics } = pageContext;
+  const monthName = convertMonthNumberToText(month);
+  const labelledMetrics = [
+    {
+      label: data.within3Days,
+      value: metrics.within3Days
+    },
+    {
+      label: data.within8Days,
+      value: metrics.within8Days
+    },
+    {
+      label: data.beyond8Days,
+      value: metrics.beyond8Days
+    }
+  ]
   return (
     <React.Fragment>
       <PracticeDetails name={name} ODSCode={ODSCode} address={address} />
       <hr />
       <h2 className="nhsuk-heading-m">
-        {month} {year}
+        {monthName} {year}
       </h2>
+      <SlaMetrics title={data.title} subtitle={data.subtitle} metrics={labelledMetrics} />
     </React.Fragment>
   );
 };
