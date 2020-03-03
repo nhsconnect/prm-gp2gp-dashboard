c7fae7a0ed6bce283d343d4a2d1eb048a6e3b6ff
Dorota-TW <dorota.sobkow@thoughtworks.com>
2020-03-03 16:05:44 +0000

    PRMT-939 [MP/DS/OK/JG] Debug deploy with smaller metrics file

--- metrics-config.js
+++ metrics-config.js
@@ -1,5 +1,5 @@
 module.exports = {
   bucket: "prm-gp2gp-dashboard-data-dev",
-  key: "v1/2020/01/practiceMetrics.json",
+  key: "practiceMetrics.json",
   outputPath: "src/data/metrics/practiceMetrics.json",
 };
