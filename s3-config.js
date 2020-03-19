1dff94bc6649da6f11a0297a61b863d2af42ac2a
Dorota-TW <dorota.sobkow@thoughtworks.com>
2020-03-19 17:57:42 +0000

    PRMT-824 [DS/OK] Update the dashboard with February data

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: "prm-gp2gp-dashboard-data-dev",
   outputPath: "src/data/practices/",
   metrics: {
-    key: "v1/2020/01/practiceMetrics.json",
+    key: "v1/2020/02/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   metadata: {
-    key: "v1/2020/01/practiceMetadata.json",
+    key: "v1/2020/02/practiceMetadata.json",
     outputFile: "practiceMetadata.json",
   },
 };
