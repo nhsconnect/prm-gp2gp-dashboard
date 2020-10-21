3a34d2102d916a279777e5b11f31aeac139cb344
Helen Zhou <helen.zhou1@nhs.net>
2020-10-21 10:57:30 +0100

    PRMT-1250 [HZ/MP] Allow for URL Parameter override for feature toggles

--- src/library/hooks/useFeatureToggle/__tests__/index.test.js
+++ src/library/hooks/useFeatureToggle/__tests__/index.test.js
@@ -3,16 +3,18 @@ import { useFeatureToggle } from "../index";
 
 jest.mock("../../../../../flags.json", () => ({
   F_NEW_FEATURE: { dev: true, prod: false },
-  F_SECOND_FEATURE: { dev: false },
+  F_SECOND_FEATURE: { dev: false, prod: true },
 }));
 
 describe("useFeatureToggle", () => {
   const ORIGINAL_LOCATION = global.window.location;
-  beforeAll(() => {
+
+  beforeEach(() => {
     delete global.window.location;
     global.window = Object.create(window);
     global.window.location = {
       hostname: "localhost",
+      search: "",
     };
   });
 
@@ -20,41 +22,86 @@ describe("useFeatureToggle", () => {
     global.window.location = ORIGINAL_LOCATION;
   });
 
-  it("returns true when requesting enabled toggle in dev environment", async () => {
-    process.env = { DEPLOYMENT_ENV: "dev" };
+  describe("configuration from flags.json", () => {
+    it("returns false when feature flag is not in flags.json", async () => {
+      const { result } = renderHook(() =>
+        useFeatureToggle("NOT_PRESENT_FEATURE")
+      );
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(false);
+    });
+    it("returns true when requesting enabled toggle in dev environment", async () => {
+      const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(true);
+    });
 
-    jest.mock(
-      "flags.dev.json",
-      () => ({
-        F_NEW_FEATURE: true,
-      }),
-      { virtual: true }
-    );
+    it("returns false when requesting disabled toggle in dev environment", async () => {
+      const { result } = renderHook(() => useFeatureToggle("F_SECOND_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(false);
+    });
 
-    const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+    it("returns false when requesting feature enabled in dev but disabled in prod", async () => {
+      global.window.location.hostname =
+        "prm-gp2gp-dashboard-dev.s3-website.eu-west-2.amazonaws.com";
+      const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(false);
+    });
 
-    const isNewFeatureEnabled = result.current;
-
-    expect(isNewFeatureEnabled).toBe(true);
+    it("returns true when requesting feature enabled in prod but disabled in dev", async () => {
+      global.window.location.hostname =
+        "prm-gp2gp-dashboard-dev.s3-website.eu-west-2.amazonaws.com";
+      const { result } = renderHook(() => useFeatureToggle("F_SECOND_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(true);
+    });
   });
 
-  it("returns false when requesting disabled toggle in dev environment", async () => {
-    const { result } = renderHook(() => useFeatureToggle("F_SECOND_FEATURE"));
+  describe("URL parameter overrides", () => {
+    it("returns false when the URL parameter overrides the file configuration", async () => {
+      global.window.location.search = "?f_new_feature=false";
+      const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(false);
+    });
 
-    const isNewFeatureEnabled = result.current;
+    it("returns true when the URL parameter overrides the file configuration", async () => {
+      global.window.location.search = "?f_second_feature=true";
+      const { result } = renderHook(() => useFeatureToggle("F_SECOND_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(true);
+    });
 
-    expect(isNewFeatureEnabled).toBe(false);
-  });
+    it("returns true when the URL parameter matches the file configuration", async () => {
+      global.window.location.search = "?f_new_feature=true";
+      const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(true);
+    });
 
-  it("returns false when requesting feature enabled in dev but disabled in prod", async () => {
-    global.window.location = {
-      hostname: "prm-gp2gp-dashboard-dev.s3-website.eu-west-2.amazonaws.com",
-    };
+    it("returns false when the URL parameter matches the file configuration", async () => {
+      global.window.location.search = "?f_second_feature=false";
+      const { result } = renderHook(() => useFeatureToggle("F_SECOND_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(false);
+    });
 
-    const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+    it("returns false when the URL parameter is true but in prod environment", async () => {
+      global.window.location.hostname =
+        "prm-gp2gp-dashboard-dev.s3-website.eu-west-2.amazonaws.com";
+      global.window.location.search = "?f_new_feature=true";
+      const { result } = renderHook(() => useFeatureToggle("F_NEW_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(false);
+    });
 
-    const isNewFeatureEnabled = result.current;
-
-    expect(isNewFeatureEnabled).toBe(false);
+    it("returns true when the URL parameter is uppercase", async () => {
+      global.window.location.search = "?F_SECOND_FEATURE=true";
+      const { result } = renderHook(() => useFeatureToggle("F_SECOND_FEATURE"));
+      const isNewFeatureEnabled = result.current;
+      expect(isNewFeatureEnabled).toBe(true);
+    });
   });
 });
--- src/library/hooks/useFeatureToggle/index.js
+++ src/library/hooks/useFeatureToggle/index.js
@@ -1,12 +1,47 @@
 import flags from "../../../../flags.json";
+import { useEffect, useState } from "react";
+
+const PROD_ENV = "prod";
+const DEV_ENV = "dev";
 
 const getEnv = () => {
   const PROD_HOSTNAME =
     "prm-gp2gp-dashboard-dev.s3-website.eu-west-2.amazonaws.com";
 
-  return window.location.hostname === PROD_HOSTNAME ? "prod" : "dev";
+  return window.location.hostname === PROD_HOSTNAME ? PROD_ENV : DEV_ENV;
+};
+
+const getUrlParam = paramName => {
+  const params = new URLSearchParams(window.location.search.toLowerCase());
+  return params.get(paramName.toLowerCase());
+};
+
+const getProdFeatureToggle = featureFlagName => {
+  if (!flags[featureFlagName]) return false;
+  return flags[featureFlagName][PROD_ENV];
+};
+
+const getDevFeatureToggle = featureFlagName => {
+  if (!flags[featureFlagName]) return false;
+
+  const urlParam = getUrlParam(featureFlagName);
+  if (urlParam === "true" || urlParam === "false") {
+    return urlParam === "true";
+  }
+
+  return flags[featureFlagName][DEV_ENV];
 };
 
 export const useFeatureToggle = featureFlagName => {
-  return flags[featureFlagName][getEnv()];
+  const [featureIsOn, setFeatureIsOn] = useState(false);
+
+  useEffect(() => {
+    if (getEnv() === PROD_ENV) {
+      setFeatureIsOn(getProdFeatureToggle(featureFlagName));
+    } else {
+      setFeatureIsOn(getDevFeatureToggle(featureFlagName));
+    }
+  }, []);
+
+  return featureIsOn;
 };
