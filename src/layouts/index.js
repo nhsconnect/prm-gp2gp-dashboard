f6563fdcb094779743bcf17752ef0ca8356af747
matthew-peers <matthew.peers2@nhs.net>
2020-10-26 10:08:06 +0000

    PRMT-1250 [HZ/MP] Fix input box shifting on focus by using shadow instead of border

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import React from "react";
 import { Helmet } from "react-helmet";
 import Header from "../components/Header";
 import "./index.scss";
