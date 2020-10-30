5e4e8808b992bc221b451634e9d963c91dd08416
Dorota-TW <dorota.sobkow@thoughtworks.com>
2020-10-30 17:09:35 +0000

    PMRT-1272 [MP/DS] Pull organisation data from v2 folder in s3

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-dashboard-data-${process.env.DEPLOYMENT_ENV}`,
   outputPath: "src/data/organisations/",
   metrics: {
-    key: "v1/2020/02/practiceMetrics.json",
+    key: "v2/2020/02/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   metadata: {
-    key: "v1/2020/02/practiceMetadata.json",
+    key: "v2/2020/02/organisationMetadata.json",
     outputFile: "organisationMetadata.json",
   },
 };
