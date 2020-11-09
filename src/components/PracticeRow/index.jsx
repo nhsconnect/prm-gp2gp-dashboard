91c3db6f65a03867024bc3959888fa063432f2db
Helen Zhou <helen.zhou1@nhs.net>
2020-11-09 18:01:17 +0000

    PMRT-1251 [HZ/DS] Fix accessibility issue with table cells

--- src/components/PracticeRow/index.jsx
+++ src/components/PracticeRow/index.jsx
@@ -5,39 +5,34 @@ import { convertToTitleCase } from "../../library/utils/convertToTitleCase/index
 import practiceTableContent from "../../data/content/practiceTable.json";
 import "./index.scss";
 
+const TableCell = ({ title, children }) => (
+  <td role="cell" className="nhsuk-table__cell">
+    <span className="nhsuk-table-responsive__heading">{title}</span>
+    {children}
+  </td>
+);
+
 const PracticeRow = ({ odsCode, name, metrics }) => {
   const formattedName = convertToTitleCase(name);
 
   return (
-    <tr className="nhsuk-table__row">
-      <td className="nhsuk-table__cell">
-        <span className="nhsuk-table-responsive__heading">
-          {practiceTableContent.firstColumnName}
-        </span>
+    <tr role="row" className="nhsuk-table__row">
+      <TableCell title={practiceTableContent.firstColumnName}>
         <Link to={`/practice/${odsCode}`}>
           {formattedName} | {odsCode}
         </Link>
-      </td>
+      </TableCell>
       {metrics && (
         <>
-          <td className="nhsuk-table__cell">
-            <span className="nhsuk-table-responsive__heading">
-              {practiceTableContent.secondColumnName}
-            </span>
+          <TableCell title={practiceTableContent.secondColumnName}>
             {metrics.within3Days}
-          </td>
-          <td className="nhsuk-table__cell">
-            <span className="nhsuk-table-responsive__heading">
-              {practiceTableContent.thirdColumnName}
-            </span>
+          </TableCell>
+          <TableCell title={practiceTableContent.thirdColumnName}>
             {metrics.within8Days}
-          </td>
-          <td className="nhsuk-table__cell">
-            <span className="nhsuk-table-responsive__heading">
-              {practiceTableContent.fourthColumnName}
-            </span>
+          </TableCell>
+          <TableCell title={practiceTableContent.fourthColumnName}>
             {metrics.beyond8Days}
-          </td>
+          </TableCell>
         </>
       )}
     </tr>
