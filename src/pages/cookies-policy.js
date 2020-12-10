2d14d49d294a1b9cb4a3e19565f6c72109da09d7
Dorota-TW <dorota.sobkow1@nhs.net>
2020-12-10 10:27:58 +0000

    PMRT-1392 [MP/DS] Add title to cookies policy page

--- src/pages/cookies-policy.js
+++ src/pages/cookies-policy.js
@@ -1,29 +1,33 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Details from "../components/Details";
 import cookiesPolicyContent from "../data/content/cookiesPolicy.json";
 
 const CookiesPolicy = () => {
   return (
-    <section>
-      <h1>{cookiesPolicyContent.title}</h1>
-      <h2>{cookiesPolicyContent.section1.heading}</h2>
-      <p>{cookiesPolicyContent.section1.text1}</p>
-      <p>{cookiesPolicyContent.section1.text2}</p>
-      <p>{cookiesPolicyContent.section1.text3}</p>
-      <h2>{cookiesPolicyContent.section2.heading}</h2>
-      <p>{cookiesPolicyContent.section2.text1}</p>
-      <ul>
-        <li>{cookiesPolicyContent.section2.list.item1}</li>
-        <li>{cookiesPolicyContent.section2.list.item2}</li>
-      </ul>
-      <p>{cookiesPolicyContent.section2.text2}</p>
-      <p>{cookiesPolicyContent.section2.text3}</p>
-      <Details
-        summary={cookiesPolicyContent.details.summary}
-        headers={cookiesPolicyContent.details.table.headers}
-        rows={cookiesPolicyContent.details.table.rows}
-      />
-    </section>
+    <>
+      <Helmet title="Cookies policy - GP2GP Service Dashboard" />
+      <section>
+        <h1>{cookiesPolicyContent.title}</h1>
+        <h2>{cookiesPolicyContent.section1.heading}</h2>
+        <p>{cookiesPolicyContent.section1.text1}</p>
+        <p>{cookiesPolicyContent.section1.text2}</p>
+        <p>{cookiesPolicyContent.section1.text3}</p>
+        <h2>{cookiesPolicyContent.section2.heading}</h2>
+        <p>{cookiesPolicyContent.section2.text1}</p>
+        <ul>
+          <li>{cookiesPolicyContent.section2.list.item1}</li>
+          <li>{cookiesPolicyContent.section2.list.item2}</li>
+        </ul>
+        <p>{cookiesPolicyContent.section2.text2}</p>
+        <p>{cookiesPolicyContent.section2.text3}</p>
+        <Details
+          summary={cookiesPolicyContent.details.summary}
+          headers={cookiesPolicyContent.details.table.headers}
+          rows={cookiesPolicyContent.details.table.rows}
+        />
+      </section>
+    </>
   );
 };
 
