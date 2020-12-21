69d3c8c5867d377fad4136301f6b31adbc838530
matthew-peers <matthew.peers2@nhs.net>
2020-12-21 16:04:44 +0000

    PRMT-1446 [HZ/MP] Introduce google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: process.env.DEPLOYMENT_ENV == "dev" ? "G-X6RDS1EV0Q" : "",
+          cookieName: "nhsuk-cookie-consent",
+          anonymize: true,
+        },
+      },
+      environments: ["production", "development"],
+    },
+    {
       resolve: "gatsby-plugin-s3",
       options: {
         bucketName: `${process.env.DEPLOYMENT_BUCKET}`,
