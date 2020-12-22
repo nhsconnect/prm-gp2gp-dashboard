b0bc9dddaa7256deffc9a843edcdc3dac3423bfe
Helen Zhou <helen.zhou1@nhs.net>
2020-12-22 11:06:41 +0000

    PRMT-1446 [HZ/MP] Removes gatsby-plugin-gdpr-cookies plugin - Is not functional with Google Analytics 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,6 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-gdpr-cookies`,
-      options: {
-        googleAnalytics: {
-          trackingId: process.env.DEPLOYMENT_ENV == "dev" ? "G-X6RDS1EV0Q" : "",
-          cookieName: "nhsuk-cookie-consent",
-          anonymize: true,
-        },
-        googleTagManager: {
-          trackingId: process.env.DEPLOYMENT_ENV == "dev" ? "G-X6RDS1EV0Q" : "",
-          cookieName: "nhsuk-cookie-consent",
-        },
-        environments: ["production", "development"],
-      },
-    },
-    {
       resolve: "gatsby-plugin-s3",
       options: {
         bucketName: `${process.env.DEPLOYMENT_BUCKET}`,
