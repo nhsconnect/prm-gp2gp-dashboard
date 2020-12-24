2783b2112bcea7641bc93c58270a94d68b03292f
Helen Zhou <helen.zhou1@nhs.net>
2020-12-24 14:51:57 +0000

    Adds ErrorBoundary React component to catch unexpected errors

---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ErrorFullback = () => (
+  <div className="nhsuk-width-container">
+    <main className="nhsuk-main-wrapper">
+      <h1>Oops, something went wrong.</h1>
+      <p>Please try again later.</p>
+    </main>
+  </div>
+);
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorFullback />;
+    }
+
+    return this.props.children;
+  }
+}
