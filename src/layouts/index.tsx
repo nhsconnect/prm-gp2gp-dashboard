593b33f2dfab2652130cc44c4d40ade603c87cac
matthew-peers <matthew.peers2@nhs.net>
2021-01-08 17:05:56 +0000

    PRMT-1430 [DS/MP] Hide cookie banner on cookie policy page

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, FC, ReactNode } from "react";
 import { useCookies } from "react-cookie";
 import { Helmet } from "react-helmet";
 
@@ -8,7 +8,7 @@ import Footer from "../components/Footer";
 import { ErrorBoundary } from "../components/ErrorBoundary";
 import "./index.scss";
 import { useFeatureToggle } from "../library/hooks/useFeatureToggle";
-import { setupAnalytics } from "../library/setupAnalytics/";
+import { setupAnalytics } from "../library/setupAnalytics";
 import getEnv from "../library/utils/getEnv";
 import analytics from "../../analytics-config.json";
 import { NHS_COOKIE_NAME } from "../library/constants";
@@ -16,10 +16,16 @@ import { NHS_COOKIE_NAME } from "../library/constants";
 const trackingId =
   getEnv() === "dev" ? analytics.trackingId.dev : analytics.trackingId.prod;
 
-const Layout = ({ path, children }) => {
+type LayoutProps = {
+  path: string;
+  childeren: ReactNode;
+};
+
+const Layout: FC<LayoutProps> = ({ path, children }) => {
   const isCookieBannerOn = useFeatureToggle("F_COOKIE_BANNER_AND_FOOTER");
   const [cookies] = useCookies([NHS_COOKIE_NAME]);
   const hasCookieConsent = cookies[NHS_COOKIE_NAME] === "true";
+  const isOnCookiePage = path === "/cookies-policy/";
 
   useEffect(() => {
     setupAnalytics({
@@ -38,7 +44,7 @@ const Layout = ({ path, children }) => {
         ></script>
       </Helmet>
       <ErrorBoundary>
-        {isCookieBannerOn && <CookieBanner path={path} />}
+        {isCookieBannerOn && !isOnCookiePage && <CookieBanner path={path} />}
         <Header />
         <div className="nhsuk-width-container">
           <main className="nhsuk-main-wrapper">{children}</main>
