00e61f4a5cff10388e7e6e6c8b70971da821f1a8
matthew-peers <matthew.peers2@nhs.net>
2021-01-25 11:58:22 +0000

    PRMT-1430 [HZ/MP] Remove cookies & footer feature toggle

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -7,7 +7,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { ErrorBoundary } from "../components/ErrorBoundary";
 import "./index.scss";
-import { useFeatureToggle } from "../library/hooks/useFeatureToggle";
 import { setupAnalytics } from "../library/setupAnalytics";
 import getEnv from "../library/utils/getEnv";
 import analytics from "../../analytics-config.json";
@@ -22,7 +21,6 @@ type LayoutProps = {
 };
 
 const Layout: FC<LayoutProps> = ({ path, children }) => {
-  const isCookieBannerOn = useFeatureToggle("F_COOKIE_BANNER_AND_FOOTER");
   const [cookies] = useCookies([NHS_COOKIE_NAME]);
   const hasCookieConsent = cookies[NHS_COOKIE_NAME] === "true";
   const isOnCookiePage = path === "/cookies-policy/";
@@ -44,12 +42,12 @@ const Layout: FC<LayoutProps> = ({ path, children }) => {
         ></script>
       </Helmet>
       <ErrorBoundary>
-        {isCookieBannerOn && !isOnCookiePage && <CookieBanner path={path} />}
+        {!isOnCookiePage && <CookieBanner path={path} />}
         <Header />
         <div className="nhsuk-width-container">
           <main className="nhsuk-main-wrapper">{children}</main>
         </div>
-        {isCookieBannerOn && <Footer />}
+        <Footer />
       </ErrorBoundary>
     </>
   );
