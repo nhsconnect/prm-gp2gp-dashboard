f4071d2d8aecf9ae5bab7f5280082de9b61c473f
Dorota-TW <dorota.sobkow1@nhs.net>
2021-01-29 16:56:14 +0000

    PRMT-1153 [MP/DS] Update s3-config to pull December data

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-dashboard-data-${process.env.DEPLOYMENT_ENV}`,
   outputPath: "src/data/organisations/",
   metrics: {
-    key: "v2/2020/02/practiceMetrics.json",
+    key: "v2/2020/12/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   metadata: {
-    key: "v2/2020/02/organisationMetadata.json",
+    key: "v2/2020/12/organisationMetadata.json",
     outputFile: "organisationMetadata.json",
   },
 };
