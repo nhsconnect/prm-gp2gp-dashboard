9d0455b944e011aac2c77a98d1b96195fdeb5c36
helen.zhou <helen.zhou1@nhs.net>
2021-02-24 14:18:35 +0000

    PRMT-1466 [HZ/MP] Update S3 metrics and metadata keys

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-dashboard-data-${process.env.DEPLOYMENT_ENV}`,
   outputPath: "src/data/organisations/",
   metrics: {
-    key: "v2/2021/1/practice-metrics.json",
+    key: "v2/2021/1/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   metadata: {
-    key: "v2/2021/1/organisation-metadata.json",
+    key: "v2/2021/1/organisationMetadata.json",
     outputFile: "organisationMetadata.json",
   },
 };
