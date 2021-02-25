05cd742c2fe9ee50ce2a4aa975b449823c0a811b
helen.zhou <helen.zhou1@nhs.net>
2021-02-25 16:39:41 +0000

    PRMT-1466 [HZ/MP] Update national metrics cut off point content

--- src/templates/national-gp2gp-statistics.tsx
+++ src/templates/national-gp2gp-statistics.tsx
@@ -37,11 +37,11 @@ const NationalStatistics: FC<NationalStatisticsProps> = ({ pageContext }) => {
       <h1>National data on GP2GP performance</h1>
       <p>The data below shows the GP2GP performance nationally</p>
       <p>
-        The data for this month extends to 8 days into the following month, so
-        that we can identify whether transfers initiated on the last day of the
-        month were integrated within the 8 day SLA. There may be some transfers
-        that have been initiated in this time period and will be integrated in
-        the future, which will not be represented in this data.
+        The data is updated monthly, 14 days after the end of the month, so that
+        we can identify whether transfers initiated on the last day of the month
+        were integrated within the 8 day SLA. There may be some transfers that
+        have been initiated in this time period and will be integrated in the
+        future, which will not be represented in this data.
       </p>
       <h2 className="nhsuk-heading-m">
         GP2GP Performance for {monthName} {year}
