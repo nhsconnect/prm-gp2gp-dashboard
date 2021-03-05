c4896e2b60bddbd5504f0be88de56e7be4734524
helen.zhou <helen.zhou1@nhs.net>
2021-03-05 17:25:47 +0000

    PRMT-1279 [HZ/EA] Add styling for the Percentage Completed table group heading

--- src/components/TableGroupHeading/index.tsx
+++ src/components/TableGroupHeading/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.scss";
 
 export const TableGroupHeading = ({
   columnGapNumber,
@@ -9,8 +10,12 @@ export const TableGroupHeading = ({
 }) => {
   return (
     <tr>
-      <th colSpan={columnGapNumber} scope="colgroup"></th>
-      <th colSpan={3} scope="colgroup">
+      <th
+        colSpan={columnGapNumber}
+        scope="colgroup"
+        className="table_group_heading__gap"
+      ></th>
+      <th colSpan={3} scope="colgroup" className="table_group_heading__header">
         {groupHeading}
       </th>
     </tr>
