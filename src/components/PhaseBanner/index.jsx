38035bbd531066d4afa5e9839afce5e636187e8f
helen.zhou <helen.zhou1@nhs.net>
2021-03-10 11:35:48 +0000

    PRMT-1417 [HZ/DS] Update GP2GP Phase banner tag to be uppercase

--- src/components/PhaseBanner/index.jsx
+++ src/components/PhaseBanner/index.jsx
@@ -4,7 +4,9 @@ import "./index.scss";
 const PhaseBanner = ({ tag, children }) => (
   <div className="gp2gp-phase-banner">
     <p className="nhsuk-body-s nhsuk-u-margin-bottom-0">
-      <strong className="nhsuk-u-margin-right-3">{tag}</strong>
+      <strong className="nhsuk-u-margin-right-3 gp2gp-phase-banner__tag">
+        {tag}
+      </strong>
       {children}
     </p>
   </div>
