e152a161dd6ab554561995e8701164b18d32c974
Dorota-TW <dorota.sobkow1@nhs.net>
2021-04-15 15:59:38 +0100

    PRMT-1895 Update the platform with March data

--- s3-config.js
+++ s3-config.js
@@ -2,15 +2,15 @@ module.exports = {
   bucket: `prm-gp2gp-dashboard-data-${process.env.DEPLOYMENT_ENV}`,
   outputPath: "src/data/organisations/",
   practiceMetrics: {
-    key: "v2/2021/2/practiceMetrics.json",
+    key: "v2/2021/3/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   metadata: {
-    key: "v2/2021/2/organisationMetadata.json",
+    key: "v2/2021/3/organisationMetadata.json",
     outputFile: "organisationMetadata.json",
   },
   nationalMetrics: {
-    key: "v2/2021/2/nationalMetrics.json",
+    key: "v2/2021/3/nationalMetrics.json",
     outputFile: "nationalMetrics.json",
   },
 };
