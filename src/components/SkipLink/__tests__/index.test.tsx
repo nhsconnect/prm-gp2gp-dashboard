df31eac3d1de93ba5bb9fd8fa1a4ca56b2d5b6ca
Dorota-TW <dorota.sobkow1@nhs.net>
2021-04-20 15:22:39 +0100

    PRMT-1534 [KV/HZ/DS] Get heading by aria role in SkipLink tests

--- src/components/SkipLink/__tests__/index.test.tsx
+++ src/components/SkipLink/__tests__/index.test.tsx
@@ -5,46 +5,52 @@ import userEvent from "@testing-library/user-event";
 
 describe("SkipLink component", () => {
   it("focuses on the heading when skip link is clicked", () => {
-    const { getByRole, getByText } = render(
-      <div>
+    const { getByRole } = render(
+      <>
         <SkipLink />
         <h1>This is a heading</h1>
-      </div>
+      </>
     );
 
     const skipLink = getByRole("link", { name: "Skip to main content" });
-    expect(getByText("This is a heading")).not.toHaveFocus();
-    expect(getByText("This is a heading")).not.toHaveAttribute("tabindex");
+    const headingElement = getByRole("heading", {
+      level: 1,
+      name: "This is a heading",
+    });
+
+    expect(headingElement).not.toHaveFocus();
+    expect(headingElement).not.toHaveAttribute("tabindex");
 
     userEvent.click(skipLink);
-    expect(getByText("This is a heading")).toHaveFocus();
-    expect(getByText("This is a heading")).toHaveAttribute("tabindex", "-1");
+    expect(headingElement).toHaveFocus();
+    expect(headingElement).toHaveAttribute("tabindex", "-1");
   });
 
   it("focuses on the heading when skip link is tabbed and entered using keyboard", () => {
-    const { getByRole, getByText } = render(
-      <div>
+    const { getByRole } = render(
+      <>
         <SkipLink />
         <h1>This is a heading</h1>
-      </div>
+      </>
     );
 
     const skipLink = getByRole("link", { name: "Skip to main content" });
-    expect(getByText("This is a heading")).not.toHaveFocus();
-    expect(getByText("This is a heading")).not.toHaveAttribute("tabindex");
+    const headingElement = getByRole("heading", {
+      level: 1,
+      name: "This is a heading",
+    });
+
+    expect(headingElement).not.toHaveFocus();
+    expect(headingElement).not.toHaveAttribute("tabindex");
 
     userEvent.tab();
-    userEvent.type(skipLink, "enter");
-    expect(getByText("This is a heading")).toHaveFocus();
-    expect(getByText("This is a heading")).toHaveAttribute("tabindex", "-1");
+    userEvent.type(skipLink, "{enter}");
+    expect(headingElement).toHaveFocus();
+    expect(headingElement).toHaveAttribute("tabindex", "-1");
   });
 
   it("doesnt set tab index anywhere if there is no heading", () => {
-    const { getByRole } = render(
-      <div>
-        <SkipLink />
-      </div>
-    );
+    const { getByRole } = render(<SkipLink />);
 
     const skipLink = getByRole("link", { name: "Skip to main content" });
     userEvent.click(skipLink);
