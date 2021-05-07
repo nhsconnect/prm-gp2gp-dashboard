3612d9676f629d58c7d7e36e0011e1cff08e9fd3
Jake Newman <jacob.newman1@nhs.net>
2021-05-07 12:01:22 +0100

    PRMT-1253 [DS/JN] Add e2e tests for table sorting on CCG page

--- e2e/__tests__/ccg.e2e.test.js
+++ e2e/__tests__/ccg.e2e.test.js
@@ -10,7 +10,7 @@ describe("CCG page", () => {
         cy.injectAxe();
       });
 
-      it("searches and navigates to the CCG page and then navigates to an individual practice page", () => {
+      it("searches and navigates to the CCG page", () => {
         cy.intercept("GET", "/organisations", organisations);
 
         cy.findByLabelText(
@@ -23,10 +23,10 @@ describe("CCG page", () => {
         cy.contains("button", "Search").click();
 
         // CCG Page
-        cy.contains("Practice performance");
-
+        cy.contains("h1", "Test CCG With GP Practices10D");
         cy.contains("Why integrate within 8 days?").click();
         cy.contains("When records are not integrated within 8 days");
+        cy.contains("Practice performance for December 2019");
 
         cy.contains("Practice name");
         cy.get('[data-testid="table__cell--row-0-col-0"]').contains(
@@ -44,6 +44,68 @@ describe("CCG page", () => {
 
         cy.contains("Beyond 8 days");
         cy.get('[data-testid="table__cell--row-0-col-4"]').contains("33.3%");
+      });
+
+      it("sort practice performance table and link to the individual practices", () => {
+        cy.intercept("GET", "/organisations", organisations);
+
+        cy.findByLabelText(
+          "Enter an ODS code, practice name or Clinical Commissioning Group (CCG) name"
+        ).type("test ccg");
+        cy.contains("li", "CCG")
+          .parent()
+          .parent()
+          .click();
+        cy.contains("button", "Search").click();
+
+        cy.contains("h1", "Test CCG With GP Practices10D");
+
+        cy.contains("Practice name");
+        cy.get('[data-testid="table__cell--row-0-col-0"]').contains(
+          "Test GP Practice With Integrations | A12345"
+        );
+        cy.get('[data-testid="table__cell--row-1-col-0"]').contains(
+          "Test GP Practice With Some Integrations | A12347"
+        );
+        cy.get('[data-testid="table__cell--row-2-col-0"]').contains(
+          "Test GP Practice With No Integrations | A12346"
+        );
+
+        cy.contains("Sort by");
+        cy.get("select#sortBySelect option:selected").should(
+          "have.text",
+          "Beyond 8 days"
+        );
+
+        cy.contains("Order");
+        cy.get("select#orderSelect option:selected").should(
+          "have.text",
+          "Descending"
+        );
+
+        cy.get("select#sortBySelect").select("Practice name");
+        cy.get("select#sortBySelect option:selected").should(
+          "have.text",
+          "Practice name"
+        );
+
+        cy.get("select#orderSelect").select("Ascending");
+        cy.get("select#orderSelect option:selected").should(
+          "have.text",
+          "Ascending"
+        );
+
+        cy.contains("Practice name");
+        cy.get('[data-testid="table__cell--row-0-col-0"]').contains(
+          "Test GP Practice With Integrations | A12345"
+        );
+        cy.get('[data-testid="table__cell--row-1-col-0"]').contains(
+          "Test GP Practice With No Integrations | A12346"
+        );
+        cy.get('[data-testid="table__cell--row-2-col-0"]').contains(
+          "Test GP Practice With Some Integrations | A12347"
+        );
+
         cy.checkAccessibility();
 
         cy.contains("a", "Test GP Practice With Integrations | A12345")
