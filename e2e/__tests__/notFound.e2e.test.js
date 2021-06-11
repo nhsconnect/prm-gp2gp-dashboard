c37fcc2ef408c0fb3848209df743894b91e440ec
Elena Alina Balusescu <eabalusescu@192.168.1.101>
2021-06-11 17:00:31 +0100

    PRMT-2018 [AB/HZ] Add metadata description to not found page

--- e2e/__tests__/notFound.e2e.test.js
+++ e2e/__tests__/notFound.e2e.test.js
@@ -4,4 +4,14 @@ describe("Not found page", () => {
     //dev and prod display different content
     cy.contains(/not found| 404 page/g);
   });
+
+  it("contains the title and description metadata for the not found page", () => {
+    cy.visit("/some-page", { failOnStatusCode: false });
+    cy.title().should("eq", "Page not found.");
+    cy.get('meta[name="description"]').should(
+      "have.attr",
+      "content",
+      "The page you are looking for on the GP Registrations Data Platform cannot be found."
+    );
+  });
 });
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,21 @@
 import React, { FC } from "react";
+import { Helmet } from "react-helmet";
 
 const NotFound: FC = () => {
   return (
-    <div className="nhsuk-u-reading-width">
-      <h1>Page not found.</h1>
-      <p>If you entered a web address please check it was correct.</p>
-    </div>
+    <>
+      <Helmet>
+        <title>Page not found.</title>
+        <meta
+          name="description"
+          content="The page you are looking for on the GP Registrations Data Platform cannot be found."
+        />
+      </Helmet>
+      <div className="nhsuk-u-reading-width">
+        <h1>Page not found.</h1>
+        <p>If you entered a web address please check it was correct.</p>
+      </div>
+    </>
   );
 };
-
 export default NotFound;
