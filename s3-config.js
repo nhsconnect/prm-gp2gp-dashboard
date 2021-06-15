36357cff4cfce0ff2ffba9c1d01699eeb680120b
Jake Newman <jacob.newman1@nhs.net>
2021-06-15 15:12:30 +0100

    PRMT-TECH [JN/HZ/DS] Bump s3 month in s3 config

--- s3-config.js
+++ s3-config.js
@@ -2,15 +2,15 @@ module.exports = {
   bucket: `prm-gp2gp-dashboard-data-${process.env.DEPLOYMENT_ENV}`,
   outputPath: "src/data/organisations/",
   practiceMetrics: {
-    key: "v2/2021/4/practiceMetrics.json",
+    key: "v2/2021/5/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   metadata: {
-    key: "v2/2021/4/organisationMetadata.json",
+    key: "v2/2021/5/organisationMetadata.json",
     outputFile: "organisationMetadata.json",
   },
   nationalMetrics: {
-    key: "v2/2021/4/nationalMetrics.json",
+    key: "v2/2021/5/nationalMetrics.json",
     outputFile: "nationalMetrics.json",
   },
 };
