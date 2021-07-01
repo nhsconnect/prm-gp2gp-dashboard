7eb558419957bcaca4e0c932a53fbe9940f2866f
K-Varsani <kavita.varsani1@nhs.net>
2021-07-01 10:21:16 +0100

    PRMT-1665 [DS/KV] add ccg A-Z link to the footer behind a feature toggle

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,34 +1,49 @@
 import React from "react";
 import { Link } from "gatsby";
 import "./index.scss";
+import { useFeatureToggles } from "../../library/hooks/useFeatureToggle";
 
-export const Footer = () => (
-  <footer role="contentinfo">
-    <div className="nhsuk-footer">
-      <div className="nhsuk-width-container">
-        <h2 className="nhsuk-u-visually-hidden">Support links</h2>
-        <ul className="nhsuk-footer__list">
-          <li className="nhsuk-footer__list-item">
-            <Link className="nhsuk-footer__list-item-link" to="/cookies-policy">
-              Cookies
-            </Link>
-          </li>
-          <li className="nhsuk-footer__list-item">
-            <Link className="nhsuk-footer__list-item-link" to="/your-privacy">
-              Your privacy
-            </Link>
-          </li>
-          <li className="nhsuk-footer__list-item">
-            <Link
-              className="nhsuk-footer__list-item-link"
-              to="/accessibility-statement"
-            >
-              Accessibility statement
-            </Link>
-          </li>
-        </ul>
-        <p className="nhsuk-footer__copyright">&copy; Crown copyright</p>
+export const Footer = () => {
+  const { showCcgAtoZLink } = useFeatureToggles();
+
+  return (
+    <footer role="contentinfo">
+      <div className="nhsuk-footer">
+        <div className="nhsuk-width-container">
+          <h2 className="nhsuk-u-visually-hidden">Support links</h2>
+          <ul className="nhsuk-footer__list">
+            <li className="nhsuk-footer__list-item">
+              <Link
+                className="nhsuk-footer__list-item-link"
+                to="/cookies-policy"
+              >
+                Cookies
+              </Link>
+            </li>
+            <li className="nhsuk-footer__list-item">
+              <Link className="nhsuk-footer__list-item-link" to="/your-privacy">
+                Your privacy
+              </Link>
+            </li>
+            <li className="nhsuk-footer__list-item">
+              <Link
+                className="nhsuk-footer__list-item-link"
+                to="/accessibility-statement"
+              >
+                Accessibility statement
+              </Link>
+            </li>
+            {showCcgAtoZLink && (
+              <li className="nhsuk-footer__list-item">
+                <Link className="nhsuk-footer__list-item-link" to="/ccgs">
+                  CCG A to Z
+                </Link>
+              </li>
+            )}
+          </ul>
+          <p className="nhsuk-footer__copyright">&copy; Crown copyright</p>
+        </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
