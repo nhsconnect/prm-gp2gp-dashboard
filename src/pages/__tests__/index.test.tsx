b986ddac6c6871f07fce269f338c9f2176f86643
Dorota-TW <dorota.sobkow1@nhs.net>
2021-07-01 17:37:57 +0100

    PRMT-1665 [DS] Mock practiceMetrics.json in the Homepage test

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -8,6 +8,21 @@ import Index from "../index";
 
 jest.mock("../../library/hooks/useJavascriptEnabled");
 
+jest.mock(
+  "../../data/organisations/practiceMetrics.json",
+  () => ({
+    practices: [
+      { odsCode: "A12345", name: "Test Practice", metrics: [] },
+      { odsCode: "X99999", name: "Second Practice", metrics: [] },
+    ],
+    ccgs: [
+      { odsCode: "12A", name: "Test CCG", practices: ["A12345"] },
+      { odsCode: "13B", name: "Second CCG", practices: ["X99999"] },
+    ],
+  }),
+  { virtual: true }
+);
+
 describe("Homepage", () => {
   it("displays organisational search when javascript is enabled", () => {
     when(mocked(useJavascriptEnabled))
