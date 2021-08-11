7b50b0689edb855bd48aea06a6d421712deda8bf
Dorota-TW <dorota.sobkow1@nhs.net>
2021-08-11 11:25:01 +0100

    PRMT-2195 [KV/DS] Use css to make the font bold in the Phase Banner instead of html strong tag

--- src/components/common/PhaseBanner/index.jsx
+++ src/components/common/PhaseBanner/index.jsx
@@ -4,9 +4,9 @@ import "./index.scss";
 export const PhaseBanner = ({ tag, children }) => (
   <div className="gp2gp-phase-banner">
     <p className="nhsuk-body-s nhsuk-u-margin-bottom-0">
-      <strong className="nhsuk-u-margin-right-3 gp2gp-phase-banner__tag">
+      <span className="nhsuk-u-margin-right-3 gp2gp-phase-banner__tag">
         {tag}
-      </strong>
+      </span>
       {children}
     </p>
   </div>
