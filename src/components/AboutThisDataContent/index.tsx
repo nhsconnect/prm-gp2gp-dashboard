153a05b42e31922357e03225be3305e5ecbd8ad4
K-Varsani <kavita.varsani1@nhs.net>
2021-08-16 12:01:30 +0100

    PRMT-2300 [JF/KV] Update 'About This Data' copy on CCG and practice page with 14 day cut off content

--- src/components/AboutThisDataContent/index.tsx
+++ src/components/AboutThisDataContent/index.tsx
@@ -9,17 +9,18 @@ export const AboutThisDataContent = () => (
     <p>
       The Data Platform is updated 14 days after the end of the month. This is
       so we can identify whether transfers started at the end of the month were
-      integrated within the 8 day SLA.
+      integrated within 8 days. When records are not integrated within 8 days,
+      GP2GP requests that a full paper copy is printed and sent to the practice.
     </p>
     <p>
-      Transfers that are pending integration when the Data Platform is updated
-      are not included, even if they are integrated at a later date.
+      Every transfer shown on the platform is categorised 14 days after it
+      started. For example, for a transfer initiated on the 2nd February, we
+      would categorise that transfer based on its state on the 15th February.
     </p>
     <p>
-      For example, a practice receives a record via GP2GP within the month of
-      May. They integrated this record on the 28th June, which is more than 14
-      days after the end of the month. This integration will not be included in
-      this data.
+      If a transfer is integrated more than 14 days after it started, it will
+      not be shown in these integration metrics. This is because it was still
+      awaiting integration when it was categorised.
     </p>
     <h3>Total integrations</h3>
     <p>All GP2GP transfers that were:</p>
@@ -28,34 +29,34 @@ export const AboutThisDataContent = () => (
         requested by the practice between the 1st and last day of the month, and
       </li>
       <li>
-        integrated (filed or suppressed) by the receiving practice by the update
-        date
+        integrated (filed or suppressed) by the receiving practice within 14
+        days
       </li>
     </ul>
     <p>It does not include GP2GP transfers that:</p>
     <ul>
       <li>failed to transfer, or</li>
       <li>were rejected by the receiving practice, or</li>
-      <li>are pending integration when the Data Platform is updated</li>
+      <li>were not integrated by the receiving practice within 14 days</li>
     </ul>
-    <h3>Within 3 days</h3>
+    <h3>Integrated within 3 days</h3>
     <p>
-      The percentage of the total integrations that were integrated (filed or
-      suppressed) within 3 days of the practice receiving the record.
+      The percentage of successful integrations that were integrated (filed or
+      suppressed) within 3 days of the record being sent.
     </p>
     <p>
       3 days is considered best practice for integrating or suppressing records
       transferred via GP2GP.
     </p>
-    <h3>Within 8 days</h3>
+    <h3>Integrated within 8 days</h3>
     <p>
-      The percentage of the total integrations that were integrated (filed or
-      suppressed) within 8 days of the practice receiving the record.
+      The percentage of successful integrations that were integrated (filed or
+      suppressed) within 8 days of the record being sent.
     </p>
-    <h3>Beyond 8 days</h3>
+    <h3>Integrated beyond 8 days</h3>
     <p>
-      The percentage of the total integrations that were integrated (filed or
-      suppressed) beyond 8 days of the practice receiving the record.
+      The percentage of successful integrations that were integrated (filed or
+      suppressed) beyond 8 days of the record being sent.
     </p>
     <p>
       When records are not integrated within 8 days, GP2GP requests that a full
