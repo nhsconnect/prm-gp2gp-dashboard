e8f5a42c7858be0a65665081539a3e93c6cafd09
Dorota-TW <dorota.sobkow1@nhs.net>
2021-08-24 16:54:46 +0100

    PRMT-1898 [JF/DS] Use PageContent component in the CCG template

--- src/templates/Ccg/__tests__/index.test.tsx
+++ src/templates/Ccg/__tests__/index.test.tsx
@@ -4,14 +4,25 @@ import { render } from "@testing-library/react";
 
 import Ccg from "..";
 import practiceMetricsMock from "../../../../__mocks__/practiceMetricsMock.json";
+import { mocked } from "ts-jest/utils";
+import { when } from "jest-when";
+import { useFeatureToggles } from "../../../library/hooks/useFeatureToggle";
+
+jest.mock("../../../library/hooks/useFeatureToggle");
 
 describe("CCG template", () => {
+  beforeEach(() => {
+    when(mocked(useFeatureToggles))
+      .calledWith()
+      .mockReturnValue({ showTabsView: true });
+  });
+  const pipelineCCGData = {
+    odsCode: "12A",
+    name: "BURTON CCG",
+    ccgPractices: practiceMetricsMock,
+  };
+
   it("renders CCG details correctly", () => {
-    const pipelineCCGData = {
-      odsCode: "12A",
-      name: "BURTON CCG",
-      ccgPractices: practiceMetricsMock,
-    };
     const expectedPracticeName = "GP Practice - A12345";
     const expectedCCGHeading = "Burton CCG - 12A";
 
@@ -19,5 +30,58 @@ describe("CCG template", () => {
 
     expect(getByText(expectedCCGHeading)).toBeInTheDocument();
     expect(getByText(expectedPracticeName)).toBeInTheDocument();
+  });
+
+  it("renders table title correctly", () => {
+    const { getByRole } = render(<Ccg pageContext={pipelineCCGData} />);
+
+    const tableTitle = getByRole("heading", {
+      name: "Integration times for February 2020",
+      level: 2,
+    });
+
+    expect(tableTitle).toBeInTheDocument();
+  });
+
+  it("renders table description correctly", () => {
+    const { getByText } = render(<Ccg pageContext={pipelineCCGData} />);
+
+    const tableDescription = getByText(
+      "The table below shows the number of records",
+      { exact: false }
+    );
+
+    expect(tableDescription).toBeInTheDocument();
+  });
+
+  it("displays CCG practices and hides about and definitions content", () => {
+    const definitionsText =
+      "The percentage of successful integrations that were integrated (filed or suppressed) within 3 days of the record being sent.";
+
+    const { getByText, queryByText, getAllByRole } = render(
+      <Ccg pageContext={pipelineCCGData} />
+    );
+
+    const allRows = getAllByRole("row");
+
+    expect(getByText("GP Practice - A12345")).toBeInTheDocument();
+    expect(getByText("Second GP Practice - A12346")).toBeInTheDocument();
+    expect(allRows[1]).toHaveTextContent("Successful integrations 21");
+    expect(allRows[1]).toHaveTextContent("Integrated within 3 days 23.8%");
+    expect(allRows[1]).toHaveTextContent("Integrated within 8 days 28.6%");
+    expect(allRows[1]).toHaveTextContent("Integrated beyond 8 days 47.6%");
+
+    expect(
+      queryByText(
+        "The Data Platform is updated 14 days after the end of the month.",
+        { exact: false }
+      )
+    ).not.toBeInTheDocument();
+
+    expect(
+      queryByText(definitionsText, {
+        exact: false,
+      })
+    ).not.toBeInTheDocument();
   });
 });
--- src/templates/Ccg/index.tsx
+++ src/templates/Ccg/index.tsx
@@ -2,9 +2,13 @@ import React, { FC } from "react";
 import { Helmet } from "react-helmet";
 
 import { OrganisationDetails } from "../../components/OrganisationDetails";
-import { CcgPageContent } from "../../components/CcgPageContent";
 import { PracticeType } from "../Practice/practice.types";
 import { convertToTitleCase } from "../../library/utils/convertToTitleCase";
+import { PageContent } from "../../components/PageContent";
+import { convertMonthNumberToText } from "../../library/utils/convertMonthNumberToText";
+import { PracticeTableWithSort } from "../../components/PracticeTableWithSort";
+import practiceTableContent from "../../data/content/practiceTable.json";
+import ccgContent from "../../data/content/ccg.json";
 
 type PageContext = {
   odsCode: string;
@@ -20,6 +24,11 @@ const Ccg: FC<CcgProps> = ({ pageContext }) => {
   const { name, odsCode, ccgPractices } = pageContext;
   const formattedName: string = convertToTitleCase(name);
 
+  const { year, month } = ccgPractices[0].metrics[0];
+  const tableTitle = `Integration times for ${convertMonthNumberToText(
+    month
+  )} ${year}`;
+
   return (
     <>
       <Helmet>
@@ -31,7 +40,19 @@ const Ccg: FC<CcgProps> = ({ pageContext }) => {
         <noscript>{`<style>.gp2gp-sort {display: none}</style>`}</noscript>
       </Helmet>
       <OrganisationDetails name={formattedName} odsCode={odsCode} />
-      <CcgPageContent ccgPractices={ccgPractices} />
+      <PageContent
+        title={tableTitle}
+        tableDescription={ccgContent.tableDescription}
+        tableContent={
+          <PracticeTableWithSort
+            ccgPractices={ccgPractices}
+            headers={practiceTableContent.headers}
+            sortBySelect={practiceTableContent.sortBySelect}
+            orderSelect={practiceTableContent.orderSelect}
+            tableCaption={tableTitle}
+          />
+        }
+      />
     </>
   );
 };
