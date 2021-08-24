7157c23b26954b7ebf90ae22b543efc737f63498
Dorota-TW <dorota.sobkow1@nhs.net>
2021-08-24 16:22:52 +0100

    PRMT-1898 [JF/DS] Add toggle for tabs in PageContent component

--- src/components/PageContent/__tests__/index.test.tsx
+++ src/components/PageContent/__tests__/index.test.tsx
@@ -1,45 +1,245 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { mocked } from "ts-jest/utils";
+import { when } from "jest-when";
 
 import { PageContent } from "../";
+import { useFeatureToggles } from "../../../library/hooks/useFeatureToggle";
+
+jest.mock("../../../library/hooks/useFeatureToggle");
 
 describe("PageContent component", () => {
-  it("should display title when passed", () => {
-    const titleText = "A title";
+  const definitionsText =
+    "The percentage of successful integrations that were integrated (filed or suppressed) within 3 days of the record being sent.";
 
-    const { getByRole } = render(
-      <PageContent title={titleText} tableDescription="" />
-    );
+  describe("with tabs", () => {
+    beforeEach(() => {
+      when(mocked(useFeatureToggles))
+        .calledWith()
+        .mockReturnValue({ showTabsView: true });
+    });
 
-    const title = getByRole("heading", { name: titleText, level: 2 });
+    it("displays title when passed", () => {
+      const titleText = "A title";
 
-    expect(title).toBeInTheDocument();
+      const { getByRole } = render(
+        <PageContent title={titleText} tableDescription="" tableContent="" />
+      );
+
+      const title = getByRole("heading", { name: titleText, level: 2 });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    it("displays table description", () => {
+      const description = "table description";
+      const { getByText } = render(
+        <PageContent title="" tableDescription={description} tableContent="" />
+      );
+
+      const tableDescription = getByText(description);
+
+      expect(tableDescription).toBeInTheDocument();
+    });
+
+    it("displays expander with the correct content", () => {
+      const { getByText } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      const expanderTitle = getByText("Why integrate within 8 days");
+      const expanderContent = getByText(
+        "This increases burden on both the sending and receiving",
+        { exact: false }
+      );
+      expect(expanderTitle).toBeInTheDocument();
+      expect(expanderContent).not.toBeVisible();
+
+      userEvent.click(expanderTitle);
+      expect(expanderContent).toBeVisible();
+    });
+
+    it("displays tab titles correctly", () => {
+      const { getByRole } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      expect(getByRole("button", { name: "Data table" })).toBeInTheDocument();
+      expect(getByRole("button", { name: "About" })).toBeInTheDocument();
+      expect(getByRole("button", { name: "Definitions" })).toBeInTheDocument();
+    });
+
+    it("displays data table content when passed", () => {
+      const content = "A data table content";
+
+      const { getByText } = render(
+        <PageContent title="" tableDescription="" tableContent={content} />
+      );
+
+      expect(getByText(content)).toBeInTheDocument();
+    });
+
+    it("displays about this data when tab is clicked", async () => {
+      const { getByRole, getByText, queryByText } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      const aboutTabContent = queryByText(
+        "The Data Platform is updated 14 days after the end of the month.",
+        { exact: false }
+      );
+      expect(aboutTabContent).not.toBeInTheDocument();
+
+      const aboutTabTitle = getByRole("button", { name: "About" });
+      userEvent.click(aboutTabTitle);
+
+      await waitFor(() => {
+        const aboutTabContent = getByText(
+          "The Data Platform is updated 14 days after the end of the month.",
+          { exact: false }
+        );
+        expect(aboutTabContent).toBeInTheDocument();
+
+        const aboutHeading = getByRole("heading", {
+          name: "About this data",
+          level: 2,
+        });
+        expect(aboutHeading).toBeInTheDocument();
+      });
+    });
+
+    it("displays definitions when tab is clicked", async () => {
+      const { getByRole, getByText, queryByText } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      const definitionsTabContent = queryByText(definitionsText, {
+        exact: false,
+      });
+      expect(definitionsTabContent).not.toBeInTheDocument();
+
+      const definitionsTabTitle = getByRole("button", { name: "Definitions" });
+      userEvent.click(definitionsTabTitle);
+
+      await waitFor(() => {
+        const definitionsTabContent = getByText(definitionsText, {
+          exact: false,
+        });
+        expect(definitionsTabContent).toBeInTheDocument();
+
+        const definitionsHeading = getByRole("heading", {
+          name: "Definitions",
+          level: 2,
+        });
+        expect(definitionsHeading).toBeInTheDocument();
+      });
+    });
   });
+  describe("without tabs - showTabsView toggled off", () => {
+    beforeEach(() => {
+      when(mocked(useFeatureToggles))
+        .calledWith()
+        .mockReturnValue({ showTabsView: false });
+    });
 
-  it("should display table description", () => {
-    const description = "table description";
-    const { getByText } = render(
-      <PageContent title="" tableDescription={description} />
-    );
+    it("displays title when passed - showTabsView toggled off", () => {
+      const titleText = "A title";
 
-    const tableDescription = getByText(description);
+      const { getByRole } = render(
+        <PageContent title={titleText} tableDescription="" tableContent="" />
+      );
 
-    expect(tableDescription).toBeInTheDocument();
-  });
+      const title = getByRole("heading", { name: titleText, level: 2 });
 
-  it("should display expander with the correct content", () => {
-    const { getByText } = render(<PageContent title="" tableDescription="" />);
+      expect(title).toBeInTheDocument();
+    });
 
-    const expanderTitle = getByText("Why integrate within 8 days");
-    const expanderContent = getByText(
-      "This increases burden on both the sending and receiving",
-      { exact: false }
-    );
-    expect(expanderTitle).toBeInTheDocument();
-    expect(expanderContent).not.toBeVisible();
+    it("displays table description - showTabsView toggled off", () => {
+      const description = "table description";
+      const { getByText } = render(
+        <PageContent title="" tableDescription={description} tableContent="" />
+      );
 
-    userEvent.click(expanderTitle);
-    expect(expanderContent).toBeVisible();
+      const tableDescription = getByText(description);
+
+      expect(tableDescription).toBeInTheDocument();
+    });
+
+    it("displays expander with the correct content - showTabsView toggled off", () => {
+      const { getByText } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      const expanderTitle = getByText("Why integrate within 8 days");
+      const expanderContent = getByText(
+        "This increases burden on both the sending and receiving",
+        { exact: false }
+      );
+      expect(expanderTitle).toBeInTheDocument();
+      expect(expanderContent).not.toBeVisible();
+
+      userEvent.click(expanderTitle);
+      expect(expanderContent).toBeVisible();
+    });
+
+    it("does not display tab titles - showTabsView toggled off", () => {
+      const { queryByRole } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      expect(
+        queryByRole("button", { name: "Data table" })
+      ).not.toBeInTheDocument();
+      expect(queryByRole("button", { name: "About" })).not.toBeInTheDocument();
+      expect(
+        queryByRole("button", { name: "Definitions" })
+      ).not.toBeInTheDocument();
+    });
+
+    it("displays data table content when passed - showTabsView toggled off", () => {
+      const content = "A data table content";
+
+      const { getByText } = render(
+        <PageContent title="" tableDescription="" tableContent={content} />
+      );
+
+      expect(getByText(content)).toBeInTheDocument();
+    });
+
+    it("displays about this data - showTabsView toggled off", () => {
+      const { getByRole, getByText } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      const aboutTabContent = getByText(
+        "The Data Platform is updated 14 days after the end of the month.",
+        { exact: false }
+      );
+      expect(aboutTabContent).toBeInTheDocument();
+
+      const aboutHeading = getByRole("heading", {
+        name: "About this data",
+        level: 2,
+      });
+      expect(aboutHeading).toBeInTheDocument();
+    });
+
+    it("displays definitions - showTabsView toggled off", async () => {
+      const { getByRole, getByText } = render(
+        <PageContent title="" tableDescription="" tableContent="" />
+      );
+
+      const definitionsTabContent = getByText(definitionsText, {
+        exact: false,
+      });
+      expect(definitionsTabContent).toBeInTheDocument();
+
+      const definitionsHeading = getByRole("heading", {
+        name: "Definitions",
+        level: 2,
+      });
+      expect(definitionsHeading).toBeInTheDocument();
+    });
   });
 });
--- src/components/PageContent/index.tsx
+++ src/components/PageContent/index.tsx
@@ -1,18 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode } from "react";
 import { Expander } from "../common/Expander";
 import eightDayExpanderContent from "../../data/content/eightDayExpander.json";
+import { Tabs } from "../Tabs";
+import { AboutThisDataContent } from "../AboutThisDataContent";
+import { DefinitionsContent } from "../Definitions";
+import { useFeatureToggles } from "../../library/hooks/useFeatureToggle";
+import "./index.scss";
 
 type PageContentProps = {
   title: string;
   tableDescription: string;
+  tableContent: ReactNode;
 };
 
 export const PageContent: FC<PageContentProps> = ({
   title,
   tableDescription,
+  tableContent,
 }) => {
+  const { showTabsView } = useFeatureToggles();
+
   return (
-    <div>
+    <section className="gp2gp-page-content-section">
       <h2>{title}</h2>
       <div className="nhsuk-u-reading-width">
         <p className="nhsuk-body">{tableDescription}</p>
@@ -27,6 +36,24 @@ export const PageContent: FC<PageContentProps> = ({
           </>
         }
       />
-    </div>
+      {showTabsView ? (
+        <Tabs
+          tabs={[
+            {
+              title: "Data table",
+              content: tableContent,
+            },
+            { title: "About", content: <AboutThisDataContent /> },
+            { title: "Definitions", content: <DefinitionsContent /> },
+          ]}
+        />
+      ) : (
+        <>
+          {tableContent}
+          <AboutThisDataContent />
+          <DefinitionsContent />
+        </>
+      )}
+    </section>
   );
 };
