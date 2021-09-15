70edb51038ebf662bcd166c7fb957ba3f1de9c4a
helen.zhou <helen.zhou1@nhs.net>
2021-09-15 12:01:24 +0100

    PRMT-2365 [HZ/DS] Update platform to august 2021

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-metrics-${process.env.DATA_BUCKET_ENV}`,
   outputPath: "src/data/organisations/",
   practiceMetrics: {
-    key: "v5/2021/7/practiceMetrics.json",
+    key: "v5/2021/8/practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   nationalMetrics: {
-    key: "v5/2021/7/nationalMetrics.json",
+    key: "v5/2021/8/nationalMetrics.json",
     outputFile: "nationalMetrics.json",
   },
 };
