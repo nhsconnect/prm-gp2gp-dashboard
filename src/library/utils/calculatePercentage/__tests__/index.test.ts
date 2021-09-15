baa792c877ac4e50ff25f0f1fcf9a7e5459f4bbb
helen.zhou <helen.zhou1@nhs.net>
2021-09-15 16:05:09 +0100

    PRMT-2366 [HZ/DS] Change calculate percentage to return number instead of string

--- src/library/utils/calculatePercentage/__tests__/index.test.ts
+++ src/library/utils/calculatePercentage/__tests__/index.test.ts
@@ -3,35 +3,28 @@ import { calculatePercentage } from "../index";
 describe("calculatePercentage", () => {
   it("rounds down to one digit", () => {
     const actual = calculatePercentage(1, 3);
-    const expected = "33.3%";
+    const expected = 33.3;
 
     expect(actual).toEqual(expected);
   });
 
   it("rounds up to one digit", () => {
     const actual = calculatePercentage(2, 3);
-    const expected = "66.7%";
+    const expected = 66.7;
 
     expect(actual).toEqual(expected);
   });
 
-  it("returns trailing 0", () => {
-    const actual = calculatePercentage(1, 2);
-    const expected = "50.0%";
-
-    expect(actual).toEqual(expected);
-  });
-
-  it("return n/a when the total is 0", () => {
+  it("return null when the total is 0", () => {
     const actual = calculatePercentage(0, 0);
-    const expected = "n/a";
+    const expected = null;
 
     expect(actual).toEqual(expected);
   });
 
-  it("return 0.0% when portion is 0", () => {
+  it("return 0 when portion is 0", () => {
     const actual = calculatePercentage(0, 1);
-    const expected = "0.0%";
+    const expected = 0;
 
     expect(actual).toEqual(expected);
   });
--- src/library/utils/calculatePercentage/index.ts
+++ src/library/utils/calculatePercentage/index.ts
@@ -1,6 +1,9 @@
-export const calculatePercentage = (portion: number, total: number): string => {
-  if (total === 0) return "n/a";
+export const calculatePercentage = (
+  portion: number,
+  total: number
+): number | null => {
+  if (total === 0) return null;
 
   const percentage = (portion / total) * 100;
-  return `${percentage.toFixed(1)}%`;
+  return Number(percentage.toFixed(1));
 };
