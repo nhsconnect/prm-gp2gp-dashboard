6f2658870388123d981485dcf8d33d4b35b0aa0b
Dorota-TW <dorota.sobkow1@nhs.net>
2021-10-15 16:22:23 +0100

    PRMT-2360 [HZ/AB/DS] Add styling for the table with extra header content

--- src/components/common/Table/index.tsx
+++ src/components/common/Table/index.tsx
@@ -18,102 +18,110 @@ export const Table: FC<TableProps> = ({
   className,
   sortedColumnIndex,
   sortOrder,
-}) => (
-  <table
-    className={classNames("gp2gp-table", className)}
-    {...(caption ? { "aria-describedby": "table-title" } : {})}
-  >
-    {caption ? (
-      <caption
-        id="table-title"
-        className={
-          caption.hidden
-            ? "nhsuk-u-visually-hidden"
-            : "nhsuk-table__caption nhsuk-u-margin-bottom-4"
-        }
-      >
-        {caption.text}
-      </caption>
-    ) : null}
-    <thead className="nhsuk-table__head">
-      <tr>
-        {headers?.map((header, columnIndex) => (
-          <th
-            scope="col"
-            role="columnheader"
-            key={`column-header-${columnIndex}`}
-            className={`nhsuk-table__col nhsuk-table__col-${columnIndex}`}
-            aria-sort={
-              sortedColumnIndex !== undefined
-                ? columnIndex === sortedColumnIndex
-                  ? sortOrder
-                  : "none"
-                : undefined
-            }
-          >
-            {header.extra ? (
-              <div className="gp2gp-table__header-with-extra">
-                <div className="gp2gp-table__header-with-extra-title">
-                  {header.title}
-                </div>
-                {header.extra}
-              </div>
-            ) : (
-              header.title
-            )}
-          </th>
-        ))}
-      </tr>
-    </thead>
-    <tbody className="nhsuk-table__body">
-      {rows?.map((row, rowIndex) => (
-        <tr role="row" className="nhsuk-table__row" key={`row-${rowIndex}`}>
-          {row?.map((cell, cellIndex) => {
-            const header = headers[cellIndex];
-            const cellData = (
-              <>
-                {" "}
-                <span className="nhsuk-table-responsive__heading">
-                  {header.title}
+}) => {
+  const isHeaderWithExtra = headers.some((header) => !!header.extra);
+
+  return (
+    <table
+      className={
+        isHeaderWithExtra
+          ? classNames("gp2gp-table-with-extra", className)
+          : classNames("gp2gp-table", className)
+      }
+      {...(caption ? { "aria-describedby": "table-title" } : {})}
+    >
+      {caption ? (
+        <caption
+          id="table-title"
+          className={
+            caption.hidden
+              ? "nhsuk-u-visually-hidden"
+              : "nhsuk-table__caption nhsuk-u-margin-bottom-4"
+          }
+        >
+          {caption.text}
+        </caption>
+      ) : null}
+      <thead className="nhsuk-table__head">
+        <tr>
+          {headers?.map((header, columnIndex) => (
+            <th
+              scope="col"
+              role="columnheader"
+              key={`column-header-${columnIndex}`}
+              className={`nhsuk-table__col nhsuk-table__col-${columnIndex}`}
+              aria-sort={
+                sortedColumnIndex !== undefined
+                  ? columnIndex === sortedColumnIndex
+                    ? sortOrder
+                    : "none"
+                  : undefined
+              }
+            >
+              {header.extra ? (
+                <div className="gp2gp-table-with-extra__header">
+                  <div className="gp2gp-table-with-extra__header-title">
+                    {header.title}
+                  </div>
                   {header.extra}
-                </span>
-                {cell}
-              </>
-            );
-            const cellProps = {
-              "data-testid": `table__cell--row-${rowIndex}-col-${cellIndex}`,
-              key: `cell-${rowIndex}-${cellIndex}`,
-              className:
-                cellIndex === sortedColumnIndex
-                  ? "nhsuk-table__cell sorted"
-                  : "nhsuk-table__cell",
-            };
-            // For accessibility the first cell in a row is a header for the screen reader
-            return cellIndex === 0 ? (
-              <th
-                {...cellProps}
-                className={`${cellProps.className} nhs-table__header_cell`}
-                scope="row"
-                role="rowheader"
-              >
-                {cellData}
-              </th>
-            ) : (
-              <td
-                {...cellProps}
-                className={
-                  header.extra
-                    ? `${cellProps.className} gp2gp-table__cell-with-extra`
-                    : cellProps.className
-                }
-                role="cell"
-              >
-                {cellData}
-              </td>
-            );
-          })}
+                </div>
+              ) : (
+                header.title
+              )}
+            </th>
+          ))}
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody className="nhsuk-table__body">
+        {rows?.map((row, rowIndex) => (
+          <tr role="row" className="nhsuk-table__row" key={`row-${rowIndex}`}>
+            {row?.map((cell, cellIndex) => {
+              const header = headers[cellIndex];
+              const cellData = (
+                <>
+                  {" "}
+                  <span className="nhsuk-table-responsive__heading">
+                    {header.title}
+                    {header.extra}
+                  </span>
+                  {cell}
+                </>
+              );
+              const cellProps = {
+                "data-testid": `table__cell--row-${rowIndex}-col-${cellIndex}`,
+                key: `cell-${rowIndex}-${cellIndex}`,
+                className:
+                  cellIndex === sortedColumnIndex
+                    ? "nhsuk-table__cell sorted"
+                    : "nhsuk-table__cell",
+              };
+              // For accessibility the first cell in a row is a header for the screen reader
+              return cellIndex === 0 ? (
+                <th
+                  {...cellProps}
+                  className={`${cellProps.className} nhs-table__header_cell`}
+                  scope="row"
+                  role="rowheader"
+                >
+                  {cellData}
+                </th>
+              ) : (
+                <td
+                  {...cellProps}
+                  className={
+                    header.extra
+                      ? `${cellProps.className} gp2gp-table-with-extra__cell`
+                      : cellProps.className
+                  }
+                  role="cell"
+                >
+                  {cellData}
+                </td>
+              );
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
