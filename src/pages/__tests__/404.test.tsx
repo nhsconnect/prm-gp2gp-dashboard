f8765f95168da32d9c2b90ef5d9c1b8673da9585
Jake Newman <jacob.newman1@nhs.net>
2021-10-27 10:29:53 +0100

    PRMT-2360 [JN/KV] Remove unused import in test

--- src/pages/__tests__/404.test.tsx
+++ src/pages/__tests__/404.test.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Index from "../index";
 import NotFound from "../404";
 import { waitFor } from "@testing-library/dom";
 
 describe("404 NotFound Page", () => {
   it("contains the title and description metadata", async () => {
-    const { getByRole } = render(<NotFound />);
+    render(<NotFound />);
     await waitFor(() => {
       expect(document.title).toEqual("Page not found.");
       expect(getMeta("description")).toEqual(
