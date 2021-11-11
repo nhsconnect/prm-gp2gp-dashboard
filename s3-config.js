9c73b9e00bfb0b0c01124f28fb7d11f68078d11e
Elena Alina Balusescu <eabalusescu@ip-192-168-1-121.eu-west-2.compute.internal>
2021-11-11 16:43:29 +0000

    PRMT-2433 [SP/AB/HZ/KV] Bump the version for National and Practice Metrics to V7

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-metrics-${process.env.DATA_BUCKET_ENV}`,
   outputPath: "src/data/organisations/",
   practiceMetrics: {
-    key: "v6/2021/9/2021-9-practiceMetrics.json",
+    key: "v7/2021/9/2021-9-practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   nationalMetrics: {
-    key: "v6/2021/9/2021-9-nationalMetrics.json",
+    key: "v7/2021/9/2021-9-nationalMetrics.json",
     outputFile: "nationalMetrics.json",
   },
 };
