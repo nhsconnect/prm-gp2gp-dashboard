b1fb89c777aab5f1245fae98ba7656892a6b69b7
Jake Newman <jacob.newman1@nhs.net>
2021-11-15 16:25:11 +0000

    PRMT-2443 [JN/DS/KV] Use October data on the dashboard (2021-10)

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-metrics-${process.env.DATA_BUCKET_ENV}`,
   outputPath: "src/data/organisations/",
   practiceMetrics: {
-    key: "v7/2021/9/2021-9-practiceMetrics.json",
+    key: "v7/2021/10/2021-10-practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   nationalMetrics: {
-    key: "v7/2021/9/2021-9-nationalMetrics.json",
+    key: "v7/2021/10/2021-10-nationalMetrics.json",
     outputFile: "nationalMetrics.json",
   },
 };
