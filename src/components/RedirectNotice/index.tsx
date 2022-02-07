9a1e3274f122fc9d7a21ad88236db6a6a683be53
helen.zhou <helen.zhou1@nhs.net>
2022-02-07 14:38:27 +0000

    PRMT-1380 [HZ] Turn showContentsList feature toggle in dev on

--- src/components/RedirectNotice/index.tsx
+++ src/components/RedirectNotice/index.tsx
@@ -8,7 +8,7 @@ type RedirectNoticeProps = {
 export const RedirectNotice: FC<RedirectNoticeProps> = ({ redirectLink }) => {
   return (
     <>
-      <h1>This page has been moved.</h1>
+      <h1 className="nhsuk-u-margin-bottom-5">This page has been moved.</h1>
       <p>
         To access, go to this <Link to={redirectLink}>link</Link>
       </p>
