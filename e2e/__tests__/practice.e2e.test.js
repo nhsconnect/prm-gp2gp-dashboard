d623f23686e212158cacc9504e91279470ec8bd5
K-Varsani <kavita.varsani1@nhs.net>
2022-02-11 16:41:01 +0000

    PRMT-1380 [DS/KV] Display redirect notice on practice page

--- e2e/__tests__/practice.e2e.test.js
+++ e2e/__tests__/practice.e2e.test.js
@@ -1,5 +1,4 @@
 const { viewPorts } = require("../support/common");
-const { practiceWithSomeIntegrations } = require("/local-mocks/mocks.js");
 
 describe("Practice page", () => {
   viewPorts.map((viewPort) => {
@@ -10,97 +9,20 @@ describe("Practice page", () => {
         cy.injectAxe();
       });
 
-      it("displays Practice page content and goes back to home page", () => {
-        cy.intercept(
-          "/ORD/2-0-0/organisations/A12347",
-          practiceWithSomeIntegrations
-        );
-
-        cy.contains("h1", "Test GP Practice With Some Integrations - A12347");
-
-        cy.title().should(
-          "eq",
-          "Test GP Practice With Some Integrations - A12347 - GP Registrations Data"
-        );
-        cy.get('meta[name="description"]').should(
-          "have.attr",
-          "content",
-          "Monthly data about GP2GP transfers for this practice"
-        );
-
-        cy.contains("A12347");
-        cy.contains("123 Some Address");
-        cy.contains("Some Town");
-        cy.contains("BL3 5DP");
-
-        cy.contains("Why integrate within 8 days").click();
-        cy.contains("If transfers are not integrated within 8 days");
-
-        cy.contains("Integration times");
-
-        cy.get("[data-testid=gp2gp-table]").within(() => {
-          cy.contains("Month");
-          cy.get("[data-testid=table__cell--row-0-col-0]").contains(
-            "December 2019"
-          );
-
-          cy.contains("Transfers received");
-          cy.get("[data-testid=table__cell--row-0-col-1]").contains(2);
-
-          cy.contains("Integrated within 3 days");
-          cy.get("[data-testid=table__cell--row-0-col-2]").contains("50%");
-
-          cy.contains("Integrated within 8 days");
-          cy.get("[data-testid=table__cell--row-0-col-3]").contains("50%");
-
-          cy.contains("Not integrated within 8 days");
-          cy.get("[data-testid=table__cell--row-0-col-4]").contains("0%");
-
-          cy.get("[data-testid=table__cell--row-1-col-0]").contains(
-            "November 2019"
-          );
-          cy.get("[data-testid=table__cell--row-1-col-1]").contains(0);
-          cy.get("[data-testid=table__cell--row-1-col-2]").contains("n/a");
-          cy.get("[data-testid=table__cell--row-1-col-3]").contains("n/a");
-          cy.get("[data-testid=table__cell--row-1-col-4]").contains("n/a");
-        });
-
-        cy.get("[data-testid=gp2gp-open-modal-btn]")
-          .filter(":visible")
-          .eq(0)
-          .click();
-        cy.contains(
-          "Total number of GP2GP transfers between the 1st and last day of the month"
-        );
-        cy.contains("button", "Close").click();
-
-        cy.contains("h3", "How we calculate the data").should("not.exist");
-        cy.contains("h2", "Definitions").should("not.exist");
-
-        cy.contains("button", "Notes about this data").click();
-        cy.contains("h3", "How we calculate the data");
-        cy.contains("December 2019").should("not.exist");
-        cy.contains("h2", "Definitions").should("not.exist");
-
-        cy.contains("button", "Definitions").click();
-        cy.contains("h2", "Definitions");
-        cy.contains("December 2019").should("not.exist");
-        cy.contains("h2", "Notes about this data").should("not.exist");
-
-        cy.contains("button", "Data table").click();
-        cy.contains("December 2019");
+      it("displays redirect notice and goes to the new integration times page", () => {
+        cy.contains("h1", "This page has been moved.");
 
         cy.checkAccessibility();
 
         cy.contains(
-          `[data-testid=back-to-search__${viewPort.device.toLowerCase()}]`,
-          "Back to search"
+          "a",
+          "Test GP Practice With Some Integrations - A12347 integration times"
         ).click();
-        cy.contains("h1", "GP2GP patient record transfers data");
+        cy.contains("h1", "Test GP Practice With Some Integrations - A12347");
       });
 
       it("displays the feedback section that links to feedback survey", () => {
-        cy.visit("/practice/A12345");
+        cy.visit("/practice/A12347");
         cy.contains("h3", "Get in touch");
         cy.contains("Take our survey").click();
         cy.contains("Feedback form for GP registrations data platform");
--- src/templates/Practice/index.tsx
+++ src/templates/Practice/index.tsx
@@ -1,23 +1,8 @@
 import React, { FC } from "react";
 import { Helmet } from "react-helmet";
-import { OrganisationAddress } from "../../components/OrganisationAddress";
-import { Table } from "../../components/common/Table";
-
-import { PracticeMetricsType, PracticeType } from "./practice.types";
-
+import { PracticeType } from "../PracticeIntegrationTimes/practice.types";
 import { convertToTitleCase } from "../../library/utils/convertToTitleCase";
-import { convertMonthNumberToText } from "../../library/utils/convertMonthNumberToText";
-import { addPercentageSign } from "../../library/utils/addPercentageSign";
-import { PageContent } from "../../components/PageContent";
-import { generateMetricsTableData } from "../../library/utils/generateMetricsTableData";
-import { HelpModal } from "../../components/common/HelpModal";
-import {
-  IntegratedWithin3DaysDefinition,
-  IntegratedWithin8DaysDefinition,
-  NotIntegratedWithin8DaysDefinition,
-  TransfersReceivedDefinition,
-  WhyIntegrateWithin8Days,
-} from "../../components/Definitions";
+import { RedirectNotice } from "../../components/RedirectNotice";
 
 type PageContext = {
   practice: PracticeType;
@@ -28,27 +13,8 @@ type PracticeProps = {
   pageContext: PageContext;
 };
 
-const generateMonthlyRowData = (metrics: PracticeMetricsType[]) => {
-  return metrics.map((metric) => {
-    const {
-      receivedCount,
-      integratedWithin3DaysPercentage,
-      integratedWithin8DaysPercentage,
-      notIntegratedWithin8DaysPercentage,
-    } = generateMetricsTableData(metric.requestedTransfers);
-
-    return [
-      `${convertMonthNumberToText(metric.month)} ${metric.year}`,
-      receivedCount,
-      addPercentageSign(integratedWithin3DaysPercentage),
-      addPercentageSign(integratedWithin8DaysPercentage),
-      addPercentageSign(notIntegratedWithin8DaysPercentage),
-    ];
-  });
-};
-
 const Practice: FC<PracticeProps> = ({ pageContext: { practice } }) => {
-  const { name, odsCode, metrics } = practice;
+  const { name, odsCode } = practice;
   const formattedName = convertToTitleCase(name);
 
   return (
@@ -59,94 +25,10 @@ const Practice: FC<PracticeProps> = ({ pageContext: { practice } }) => {
           name="description"
           content="Monthly data about GP2GP transfers for this practice"
         />
-        <noscript>{`<style>.gp2gp-tabs,.gp2gp-open-modal-btn {display: none}</style>`}</noscript>
       </Helmet>
-      <h1 className="nhsuk-u-margin-bottom-5">
-        {formattedName ? `${formattedName} - ${odsCode}` : odsCode}
-      </h1>
-      <OrganisationAddress odsCode={odsCode} />
-      <hr aria-hidden={true} />
-
-      <PageContent
-        title="Integration times for registering practice"
-        tableDescription={
-          <>
-            <p>
-              The table below shows the integration times for GP2GP transfers
-              received.
-            </p>
-          </>
-        }
-        tableContent={
-          <Table
-            headers={[
-              { title: "Month " },
-              {
-                title: "GP2GP Transfers received ",
-                extra: (
-                  <HelpModal
-                    ariaLabelledBy="transfers-received-modal-title"
-                    iconHiddenDescription="Open modal with definition"
-                    content={
-                      <TransfersReceivedDefinition ariaLabelId="transfers-received-modal-title" />
-                    }
-                  />
-                ),
-              },
-              {
-                title: "Integrated within 3 days ",
-                extra: (
-                  <HelpModal
-                    ariaLabelledBy="integrated-within-3-days-modal-title"
-                    iconHiddenDescription="Open modal with definition"
-                    content={
-                      <IntegratedWithin3DaysDefinition ariaLabelId="integrated-within-3-days-modal-title" />
-                    }
-                  />
-                ),
-              },
-              {
-                title: "Integrated within 8 days ",
-                extra: (
-                  <HelpModal
-                    ariaLabelledBy="integrated-within-8-days-modal-title"
-                    iconHiddenDescription="Open modal with definition"
-                    content={
-                      <IntegratedWithin8DaysDefinition ariaLabelId="integrated-within-8-days-modal-title" />
-                    }
-                  />
-                ),
-              },
-              {
-                title: (
-                  <>
-                    Not integrated within 8 days{" "}
-                    <div className="gp2gp-title-emphasis">
-                      (paper copy requested){" "}
-                    </div>
-                  </>
-                ),
-                extra: (
-                  <HelpModal
-                    ariaLabelledBy="not-integrated-within-8-days-modal-title"
-                    iconHiddenDescription="Open modal with definition"
-                    content={
-                      <>
-                        <NotIntegratedWithin8DaysDefinition ariaLabelId="not-integrated-within-8-days-modal-title" />
-                        <WhyIntegrateWithin8Days title="Why integrate within 8 days?" />
-                      </>
-                    }
-                  />
-                ),
-              },
-            ]}
-            caption={{
-              text: "Integration times for the recent months",
-              hidden: true,
-            }}
-            rows={generateMonthlyRowData(metrics)}
-          />
-        }
+      <RedirectNotice
+        redirectLink={`/practice/${odsCode}/integration-times`}
+        linkText={`${formattedName} - ${odsCode} integration times`}
       />
     </>
   );
