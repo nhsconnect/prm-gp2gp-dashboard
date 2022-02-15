52a33fa0321fc93d30089b8fa89a91c7b39b1896
Alina Balusescu <alina.balusescu1@nhs.net>
2022-02-15 11:58:09 +0000

    PRMT-2564 [HZ/SP/DS/AB] Update metrics version for January

--- s3-config.js
+++ s3-config.js
@@ -2,11 +2,11 @@ module.exports = {
   bucket: `prm-gp2gp-metrics-${process.env.DATA_BUCKET_ENV}`,
   outputPath: "src/data/organisations/",
   practiceMetrics: {
-    key: "v9/2021/12/2021-12-practiceMetrics.json",
+    key: "v9/2022/1/2022-1-practiceMetrics.json",
     outputFile: "practiceMetrics.json",
   },
   nationalMetrics: {
-    key: "v9/2021/12/2021-12-nationalMetrics.json",
+    key: "v9/2022/1/2022-1-nationalMetrics.json",
     outputFile: "nationalMetrics.json",
   },
 };
