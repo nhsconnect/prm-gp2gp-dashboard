6ba683ae5a219020af326b5dfea7488698ae1aa0
K-Varsani <kavita.varsani1@nhs.net>
2022-02-16 12:52:22 +0000

    PRMT-1415 [KV] Add definitions for Transfers requested page

--- src/components/Definitions/index.tsx
+++ src/components/Definitions/index.tsx
@@ -143,6 +143,113 @@ export const WhyIntegrateWithin8Days: FC<WhyIntegrateWithin8DaysProps> = ({
   </>
 );
 
+export const RegistrationsTriggeredByGP2GPDefinition: FC<DefinitionProps> = ({
+  ariaLabelId,
+}) => (
+  <>
+    <h3 id={ariaLabelId}>Registrations that triggered GP2GP transfer</h3>
+    <p>
+      Total number of registrations that triggered a GP2GP transfer between the
+      1st and the last day of the month.
+    </p>
+    <p>
+      <strong>Not Included</strong>
+    </p>
+    <p>
+      This does not include registrations that did not trigger a GP2GP transfer.
+      This could be for one of the following reasons
+    </p>
+    <ul>
+      <li>registrations that are not eligible for GP2GP. For example:</li>
+      <ul>
+        <li>the previous practice was in Wales or Scotland</li>
+        <li>registrations to and from the armed forces or justice system</li>
+        <li>there is no previous practice registered, for any reason</li>
+      </ul>
+      <li>
+        registrations that had a technical issue before GP2GP was triggered. For
+        example, if there was a technical issue during PDS trace or update.
+      </li>
+      <li>transfers between SystmOne practices that do not go via GP2GP.</li>
+    </ul>
+    <p>
+      This list is not exhaustive and there may be other scenarios in which a
+      GP2GP transfer is not triggered. For more information about how the data
+      is calculated see the ‘Notes about this data’ section.
+    </p>
+  </>
+);
+
+export const TransfersReceivedPercentageDefinition: FC<DefinitionProps> = ({
+  ariaLabelId,
+}) => (
+  <>
+    <h3 id={ariaLabelId}>GP2GP transfers received</h3>
+    <p>
+      Percentage of GP2GP transfers between the 1st and last day of the month
+      that were successfully received by the registering practice.
+    </p>
+    <p>
+      This <strong>includes</strong> transfers that have not been integrated.
+    </p>
+    <p>
+      This <strong>does not include</strong> GP2GP transfers that
+    </p>
+    <ul>
+      <li>failed to transfer, or</li>
+      <li>were rejected by the receiving practice</li>
+    </ul>
+    <p>
+      For information about how the data is calculated see the 'Notes about this
+      data' section.
+    </p>
+  </>
+);
+
+export const GP2GPTechnicalFailuresDefinition: FC<DefinitionProps> = ({
+  ariaLabelId,
+}) => (
+  <>
+    <h3 id={ariaLabelId}>
+      GP2GP technical failures{" "}
+      <span className="gp2gp-title-emphasis">(paper copy requested)</span>
+    </h3>
+    <p>
+      Percentage of GP2GP transfers requested between the 1st and last day of
+      the month that failed for a technical reason.
+    </p>
+    <p>
+      <strong>Reasons for technical failures</strong>
+    </p>
+    <p>
+      Technical failures can happen from the point the GP2GP transfer is
+      triggered until the point of integration. Technical failures are not
+      caused by user actions and should be reported to the system supplier.
+    </p>
+    <p>Example reasons for technical failures</p>
+    <ul>
+      <li>Large message failures</li>
+      <li>Spine errors</li>
+      <li>Timeouts</li>
+    </ul>
+    <p>
+      This list is not exhaustive and there are other scenarios in which a
+      technical failure happens.
+    </p>
+    <p>
+      <strong>Not included</strong>
+    </p>
+    <p>
+      Technical issues that happened before GP2GP was triggered. For example, if
+      there was a technical issue during PDS trace or update.
+    </p>
+    <p>
+      For information about how the data is calculated see the 'Notes about this
+      data' section.
+    </p>
+  </>
+);
+
 export const IntegrationsDefinitionsContent = () => (
   <div className="nhsuk-u-reading-width">
     <h2 className="nhsuk-u-margin-top-6">Definitions</h2>
@@ -150,5 +257,14 @@ export const IntegrationsDefinitionsContent = () => (
     <IntegratedWithin3DaysDefinition />
     <IntegratedWithin8DaysDefinition />
     <NotIntegratedWithin8DaysDefinition />
+  </div>
+);
+
+export const TransfersRequestedDefinitionsContent = () => (
+  <div className="nhsuk-u-reading-width">
+    <h2 className="nhsuk-u-margin-top-6">Definitions</h2>
+    <RegistrationsTriggeredByGP2GPDefinition />
+    <TransfersReceivedPercentageDefinition />
+    <GP2GPTechnicalFailuresDefinition />
   </div>
 );
