06762b86a06cd2bd823e123e033bcee906446a46
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-02-21 15:37:53 +0000

    PRMT-1415 [SP] Add PracticeTransfersRequestedTableWithSort for CCG transfers requested template

---
+++ src/components/PracticeTransfersRequestedTableWithSort/__tests__/index.test.tsx
@@ -0,0 +1,224 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { PracticeTransfersRequestedTableWithSort } from "../";
+import { SortOrder } from "../index";
+import practiceMetricsPercentageMock from "../../../../__mocks__/practiceTransfersRequestedMetricsPercentageMock.json";
+import practiceTableContent from "../../../data/content/practiceTransfersRequestedTable.json";
+
+const tableHeaders = [
+  { title: "Requesting practice name " },
+  { title: "Transfers received " },
+  { title: "GP2GP transfers received " },
+  { title: "GP2GP technical failures " },
+];
+
+describe("PracticeTableWithSort component", () => {
+  it("should display table heading caption with the month and year", () => {
+    const tableCaptionText =
+      "Integration times for registering practices - February 2020";
+
+    const { getByText } = render(
+      <PracticeTransfersRequestedTableWithSort
+        ccgPractices={practiceMetricsPercentageMock}
+        headers={tableHeaders}
+        sortBySelect={practiceTableContent.sortBySelect}
+        orderSelect={practiceTableContent.orderSelect}
+        tableCaption={tableCaptionText}
+      />
+    );
+
+    const tableCaption = getByText(`${tableCaptionText}`);
+
+    expect(tableCaption).toBeInTheDocument();
+  });
+
+  it("displays practices ordered by Technical Failures Percentage SLA by default in descending order", () => {
+    const { getAllByRole, getByRole } = render(
+      <PracticeTransfersRequestedTableWithSort
+        ccgPractices={practiceMetricsPercentageMock}
+        headers={tableHeaders}
+        sortBySelect={practiceTableContent.sortBySelect}
+        orderSelect={practiceTableContent.orderSelect}
+        tableCaption={"Some table title"}
+      />
+    );
+
+    const allRows = getAllByRole("row");
+
+    const sortBySelect = getByRole("combobox", {
+      name: `Sort by${practiceTableContent.selectHiddenLabel}`,
+    });
+    const orderSelect = getByRole("combobox", {
+      name: `Order${practiceTableContent.selectHiddenLabel}`,
+    });
+    expect(sortBySelect).toHaveValue("technicalFailuresPercentage");
+    expect(orderSelect).toHaveValue(SortOrder.DESCENDING);
+
+    expect(allRows[1]).toHaveTextContent("GP2GP technical failures 28.6%");
+    expect(allRows[2]).toHaveTextContent("GP2GP technical failures 15%");
+    expect(allRows[3]).toHaveTextContent("GP2GP technical failures 7.7%");
+    expect(allRows[4]).toHaveTextContent("GP2GP technical failures 7.1%");
+    expect(allRows[5]).toHaveTextContent("GP2GP technical failures 0%");
+    expect(allRows[6]).toHaveTextContent("GP2GP technical failures n/a");
+    expect(allRows.length).toBe(7);
+  });
+
+  it("navigates to a practice page when a link is clicked", () => {
+    const { getByRole } = render(
+      <PracticeTransfersRequestedTableWithSort
+        ccgPractices={practiceMetricsPercentageMock}
+        headers={tableHeaders}
+        sortBySelect={practiceTableContent.sortBySelect}
+        orderSelect={practiceTableContent.orderSelect}
+        tableCaption={"Some table title"}
+      />
+    );
+
+    const practicePageLink = getByRole("link", {
+      name: "GP Practice - A12345",
+    });
+
+    expect(practicePageLink.getAttribute("href")).toBe(
+      "/practice/A12345/transfers-requested"
+    );
+  });
+
+  describe("Sorting practice table", () => {
+    const cases = [
+      [
+        "Requesting practice name",
+        "requestingPracticeName",
+        SortOrder.DESCENDING,
+        [
+          "Requesting practice name Third GP Practice - A12347",
+          "Requesting practice name Sixth GP Practice - A12350",
+          "Requesting practice name Second GP Practice - A12346",
+        ],
+      ],
+      [
+        "Requesting practice name",
+        "requestingPracticeName",
+        SortOrder.ASCENDING,
+        [
+          "Requesting practice name Fifth GP Practice - A12349",
+          "Requesting practice name Fourth GP Practice - A12348",
+          "Requesting practice name GP Practice - A12345",
+        ],
+      ],
+      [
+        "Transfers received",
+        "requestedCount",
+        SortOrder.DESCENDING,
+        [
+          "Transfers received 40",
+          "Transfers received 22",
+          "Transfers received 14",
+        ],
+      ],
+      [
+        "Transfers received",
+        "requestedCount",
+        SortOrder.ASCENDING,
+        [
+          "Transfers received 0",
+          "Transfers received 7",
+          "Transfers received 13",
+        ],
+      ],
+      [
+        "GP2GP transfers received",
+        "receivedPercentage",
+        SortOrder.DESCENDING,
+        [
+          "GP2GP transfers received 100%",
+          "GP2GP transfers received 92.9%",
+          "GP2GP transfers received 92.3%",
+        ],
+      ],
+      [
+        "GP2GP transfers received",
+        "receivedPercentage",
+        SortOrder.ASCENDING,
+        [
+          "GP2GP transfers received n/a",
+          "GP2GP transfers received 71.4%",
+          "GP2GP transfers received 85%",
+        ],
+      ],
+      [
+        "GP2GP technical failures",
+        "technicalFailuresPercentage",
+        SortOrder.DESCENDING,
+        [
+          "GP2GP technical failures 28.6%",
+          "GP2GP technical failures 15%",
+          "GP2GP technical failures 7.7%",
+        ],
+      ],
+      [
+        "GP2GP technical failures",
+        "technicalFailuresPercentage",
+        SortOrder.ASCENDING,
+        [
+          "GP2GP technical failures n/a",
+          "GP2GP technical failures 0%",
+          "GP2GP technical failures 7.1%",
+        ],
+      ],
+    ];
+
+    it.each(cases)(
+      "displays practices ordered by %p field and %p %p order when selected",
+      (columnHeader, fieldName, order, expectedSortOrder) => {
+        const { getAllByRole, getByRole, queryAllByRole } = render(
+          <PracticeTransfersRequestedTableWithSort
+            ccgPractices={practiceMetricsPercentageMock}
+            headers={tableHeaders}
+            sortBySelect={practiceTableContent.sortBySelect}
+            orderSelect={practiceTableContent.orderSelect}
+            tableCaption={"Some table title"}
+          />
+        );
+
+        const allRows = getAllByRole("row");
+
+        const sortBySelect = getByRole("combobox", {
+          name: `Sort by${practiceTableContent.selectHiddenLabel}`,
+        });
+        const orderSelect = getByRole("combobox", {
+          name: `Order${practiceTableContent.selectHiddenLabel}`,
+        });
+
+        userEvent.selectOptions(sortBySelect, fieldName);
+        userEvent.selectOptions(orderSelect, order);
+
+        expect(sortBySelect).toHaveValue(fieldName);
+        expect(orderSelect).toHaveValue(order);
+
+        (expectedSortOrder as string[]).forEach((cell, index) => {
+          const sortedCell = queryAllByRole("cell", {
+            name: cell,
+          });
+
+          const sortedRowheader = queryAllByRole("rowheader");
+
+          const expectedSortedCell = sortedCell[0] || sortedRowheader[index];
+
+          expect(expectedSortedCell).toHaveClass("sorted");
+        });
+
+        const sortedColumnHeader = getByRole("columnheader", {
+          name: columnHeader as string,
+        });
+        expect(sortedColumnHeader.getAttribute("aria-sort")).toBe(order);
+
+        expect(allRows[1]).toHaveTextContent(expectedSortOrder[0]);
+        expect(allRows[2]).toHaveTextContent(expectedSortOrder[1]);
+        expect(allRows[3]).toHaveTextContent(expectedSortOrder[2]);
+        expect(allRows.length).toBe(7);
+      }
+    );
+  });
+});
---
+++ src/components/PracticeTransfersRequestedTableWithSort/index.tsx
@@ -0,0 +1,138 @@
+import React, { AriaAttributes, FC, useMemo, useState, ReactNode } from "react";
+import { Link } from "gatsby";
+import { Table } from "../common/Table";
+import { Select } from "../common/Select";
+
+import { PracticePercentageType } from "../../library/utils/generateTransfersRequestedMetricsTableData";
+
+import { addPercentageSign } from "../../library/utils/addPercentageSign";
+import { convertToTitleCase } from "../../library/utils/convertToTitleCase";
+
+import practiceTableContent from "../../data/content/practiceTransfersRequestedTable.json";
+import "../common/Table/index.scss";
+
+type TableWithSortProps = {
+  ccgPractices: PracticePercentageType[];
+  headers: { title: ReactNode; extra?: ReactNode }[];
+  sortBySelect: SelectType;
+  orderSelect: SelectType;
+  tableCaption: string;
+};
+
+type SelectType = {
+  defaultValue: string;
+  options: { displayText: string; value: string }[];
+};
+
+export const SortOrder = {
+  DESCENDING: practiceTableContent.orderSelect.options[0].value,
+  ASCENDING: practiceTableContent.orderSelect.options[1].value,
+};
+
+const PracticeLink = ({ odsCode, name }: { odsCode: string; name: string }) => {
+  const formattedName = convertToTitleCase(name);
+  return (
+    <Link to={`/practice/${odsCode}/transfers-requested`}>
+      {formattedName} - {odsCode}
+    </Link>
+  );
+};
+
+const sortPractices = (
+  practices: PracticePercentageType[],
+  fieldName: string,
+  order: string
+) => {
+  const getFieldValue = (field: any) => {
+    if (fieldName === "requestingPracticeName") return field.name;
+    const transfersReceivedMetrics = field.metrics[0].requestedTransfers;
+    return transfersReceivedMetrics[fieldName];
+  };
+
+  const sortData = (firstEl: string | number, secondEl: string | number) => {
+    if (firstEl === null || secondEl > firstEl) {
+      return -1;
+    }
+    if (secondEl === null || secondEl < firstEl) {
+      return 1;
+    }
+    return 0;
+  };
+
+  return [...practices].sort((firstEl, secondEl) => {
+    const firstField = getFieldValue(firstEl);
+    const secondField = getFieldValue(secondEl);
+    if (order === SortOrder.ASCENDING) {
+      return sortData(firstField, secondField);
+    }
+    return sortData(secondField, firstField);
+  });
+};
+
+export const PracticeTransfersRequestedTableWithSort: FC<TableWithSortProps> =
+  ({ ccgPractices, headers, sortBySelect, orderSelect, tableCaption }) => {
+    const [selectedField, setSelectedField] = useState(
+      sortBySelect.defaultValue
+    );
+    const [selectedOrder, setSelectedOrder] = useState(
+      orderSelect.defaultValue
+    );
+    const sortedPractices = useMemo(() => {
+      return sortPractices(ccgPractices, selectedField, selectedOrder);
+    }, [ccgPractices, selectedField, selectedOrder]);
+
+    const practiceTableRows = sortedPractices.map(
+      ({ odsCode, name, metrics }: PracticePercentageType) => {
+        const requestedMetric = metrics[0].requestedTransfers;
+        return [
+          <PracticeLink odsCode={odsCode} name={name} />,
+          requestedMetric.requestedCount,
+          addPercentageSign(requestedMetric.receivedPercentage),
+          addPercentageSign(requestedMetric.technicalFailuresPercentage),
+        ];
+      }
+    );
+
+    const handleSortByValueChange = (value: string) => {
+      setSelectedField(value);
+    };
+
+    const handleOrderValueChange = (value: string) => {
+      setSelectedOrder(value);
+    };
+
+    const sortedColumnIndex = sortBySelect.options.findIndex(
+      (option) => option.value === selectedField
+    );
+
+    return (
+      <div className="nhsuk-u-margin-top-6">
+        <div className="gp2gp-sort">
+          <Select
+            label="Sort by"
+            hiddenLabel={practiceTableContent.selectHiddenLabel}
+            options={sortBySelect.options}
+            id="sortBySelect"
+            defaultValue={sortBySelect.defaultValue}
+            handleValueChange={handleSortByValueChange}
+            className="nhsuk-u-margin-right-6"
+          />
+          <Select
+            label="Order"
+            hiddenLabel={practiceTableContent.selectHiddenLabel}
+            options={orderSelect.options}
+            id="orderSelect"
+            defaultValue={orderSelect.defaultValue}
+            handleValueChange={handleOrderValueChange}
+          />
+        </div>
+        <Table
+          caption={{ text: `${tableCaption}`, hidden: false }}
+          headers={headers}
+          rows={practiceTableRows}
+          sortedColumnIndex={sortedColumnIndex}
+          sortOrder={selectedOrder as AriaAttributes["aria-sort"]}
+        />
+      </div>
+    );
+  };
