945b8fa2cf3cfeccce8d1b090280e84227bd80f1
Jake Newman <jacob.newman1@nhs.net>
2022-02-23 14:04:08 +0000

    PRMT-2495 [JN/AB] Return selected radio values from RadioButtonsForm

--- src/components/common/FormComponents/RadioButtonsForm/__tests__/index.test.tsx
+++ src/components/common/FormComponents/RadioButtonsForm/__tests__/index.test.tsx
@@ -1,25 +1,36 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import React from "react";
 import userEvent from "@testing-library/user-event";
 import { RadioButtonsForm } from "../index";
 
 describe("RadioButtonsForm", () => {
-  it("renders form and calls onSubmit when submit button is clicked", () => {
-    const handleSubmitButton = jest
-      .fn()
-      .mockImplementation((e) => e.preventDefault());
+  it("renders form and calls handleSubmit when submit button is clicked", () => {
+    const onSubmitMock = jest.fn();
     const submitText = "submitText";
 
     const { getByRole } = render(
-      <RadioButtonsForm
-        submitText={submitText}
-        handleSubmit={handleSubmitButton}
-      />
+      <RadioButtonsForm submitText={submitText} onSubmit={onSubmitMock} />
     );
 
     const submitButton = getByRole("button", { name: submitText });
     userEvent.click(submitButton);
 
-    expect(handleSubmitButton).toBeCalledTimes(1);
+    expect(onSubmitMock).toBeCalledTimes(1);
+  });
+
+  it("submits form with default value of a radio button", () => {
+    const onSubmitMock = jest.fn();
+    const submitText = "submitText";
+
+    const { getByTestId } = render(
+      <RadioButtonsForm submitText={submitText} onSubmit={onSubmitMock} />
+    );
+
+    fireEvent.submit(getByTestId("nhsuk-radio-buttons-form"));
+
+    expect(onSubmitMock).toBeCalledTimes(1);
+    expect(onSubmitMock).toBeCalledWith({
+      "radio-input-1": "Transfers requested",
+    });
   });
 });
--- src/components/common/FormComponents/RadioButtonsForm/index.tsx
+++ src/components/common/FormComponents/RadioButtonsForm/index.tsx
@@ -1,17 +1,47 @@
-import React, { FC } from "react";
+import React, { FC, FormEvent } from "react";
 
 type RadioButtonsFormProps = {
   submitText: string;
-  handleSubmit: () => void;
+  onSubmit: (event: { [k: string]: FormDataEntryValue }) => void;
 };
 
 export const RadioButtonsForm: FC<RadioButtonsFormProps> = ({
   submitText,
-  handleSubmit,
+  onSubmit,
 }) => {
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formProps = Object.fromEntries(formData);
+    onSubmit(formProps);
+  }
+
   return (
-    <div className={"nhsuk-form-group"}>
-      <form onSubmit={handleSubmit}>
+    <div className={"nhsuk-form-group nhsuk-radio-buttons-form"}>
+      <form onSubmit={handleSubmit} data-testid="nhsuk-radio-buttons-form">
+        <legend className="gp2gp-fieldset__legend nhsuk-u-margin-bottom-4 nhsuk-u-padding-top-2">
+          <h3 className="nhsuk-fieldset__heading ">
+            Which dataset you would like to download?
+          </h3>
+        </legend>
+        <div className="nhsuk-radios">
+          <div className="nhsuk-radios__item" key={"radio-input-datasets"}>
+            <input
+              className="nhsuk-radios__input"
+              id="radio-input-datasets"
+              type="radio"
+              name="radio-input-1"
+              value="Transfers requested"
+              defaultChecked={true}
+            />
+            <label
+              className="nhsuk-label nhsuk-radios__label"
+              htmlFor={"radio-input-datasets"}
+            >
+              {"Transfers requested"}
+            </label>
+          </div>
+        </div>
         <button type="submit">{submitText}</button>
       </form>
     </div>
