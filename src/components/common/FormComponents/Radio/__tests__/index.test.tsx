cadeaac39dad9079c81fc44b01410e9a71f12178
Alina Balusescu <alina.balusescu1@nhs.net>
2022-02-24 12:18:36 +0000

    PRMT-2495 [SP/AB] Add Radio component and remove RadioButtonsForm

---
+++ src/components/common/FormComponents/Radio/__tests__/index.test.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { getByRole, render } from "@testing-library/react";
+import { Radio } from "../";
+
+describe("Radio component", () => {
+  it("displays the title", () => {
+    const title = "This is a radio section";
+
+    const { getByText } = render(
+      <Radio
+        title={title}
+        options={[]}
+        radioId="radio-id-1"
+        selectedValue="option-1"
+        setSelectedValue={() => {}}
+      />
+    );
+
+    expect(getByText("This is a radio section")).toBeInTheDocument();
+  });
+
+  it("displays one option with unique optionId and unique radioId", () => {
+    const options = [{ displayValue: "First option", value: "option-1" }];
+
+    const { getByRole } = render(
+      <Radio
+        title=""
+        options={options}
+        radioId="radio-id-1"
+        selectedValue="option-1"
+        setSelectedValue={() => {}}
+      />
+    );
+
+    const radioOption = getByRole("radio", { name: "First option" });
+
+    expect(radioOption).toBeInTheDocument();
+    expect(radioOption).toHaveAttribute("id", "radio-id-1-option-0");
+    expect(radioOption).toHaveAttribute("name", "radio-input-radio-id-1");
+  });
+
+  it("displays multiple options", () => {
+    const options = [
+      { displayValue: "First option", value: "option-1" },
+      { displayValue: "Second option", value: "option-2" },
+      { displayValue: "Third option", value: "option-3" },
+    ];
+
+    const { getAllByRole } = render(
+      <Radio
+        title=""
+        options={options}
+        radioId="radio-id-1"
+        selectedValue="option-1"
+        setSelectedValue={() => {}}
+      />
+    );
+
+    const radioOptions = getAllByRole("radio");
+
+    expect(radioOptions.length).toBe(3);
+    expect(radioOptions[0].nextSibling).toHaveTextContent("First option");
+    expect(radioOptions[1].nextSibling).toHaveTextContent("Second option");
+    expect(radioOptions[2].nextSibling).toHaveTextContent("Third option");
+  });
+
+  it("displays multiple options with default checked options", () => {
+    const options = [
+      { displayValue: "First option", value: "option-1" },
+      { displayValue: "Second option", value: "option-2" },
+      { displayValue: "Third option", value: "option-3" },
+    ];
+    const callback = jest.fn();
+    const { getByRole } = render(
+      <Radio
+        title=""
+        options={options}
+        radioId="radio-id-1"
+        selectedValue={"option-2"}
+        setSelectedValue={callback}
+      />
+    );
+
+    const firstOption = getByRole("radio", { name: "First option" });
+    const secondOption = getByRole("radio", { name: "Second option" });
+
+    expect(firstOption).not.toBeChecked();
+    expect(secondOption).toBeChecked();
+  });
+
+  it("calls callback with default option value when button pressed", () => {
+    const options = [
+      { displayValue: "First option", value: "option-1" },
+      { displayValue: "Second option", value: "option-2" },
+    ];
+    const callback = jest.fn();
+
+    const { getByText } = render(
+      <Radio
+        title=""
+        options={options}
+        radioId="radio-id-1"
+        selectedValue="option-1"
+        setSelectedValue={callback}
+      />
+    );
+    const selectedData = getByText("Second option");
+    selectedData.click();
+
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith("option-2");
+  });
+});
---
+++ src/components/common/FormComponents/Radio/index.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from "react";
+import "./index.scss";
+
+type RadioOption = {
+  displayValue: string;
+  value: string;
+};
+
+type RadioProps = {
+  title: string;
+  radioId: string;
+  options: RadioOption[];
+  selectedValue: string;
+  setSelectedValue: (value: string) => void;
+};
+
+export const Radio: FC<RadioProps> = ({
+  title,
+  radioId,
+  options,
+  selectedValue,
+  setSelectedValue,
+}) => {
+  return (
+    <fieldset className="gp2gp-fieldset">
+      <legend className="gp2gp-fieldset__legend nhsuk-u-margin-bottom-4 nhsuk-u-padding-top-2">
+        <h3 className="nhsuk-fieldset__heading ">{title}</h3>
+      </legend>
+      <div className="nhsuk-radios">
+        {options.map((option, index) => {
+          const optionId = `${radioId}-option-${index}`;
+          return (
+            <div className="nhsuk-radios__item" key={optionId}>
+              <input
+                className="nhsuk-radios__input"
+                id={optionId}
+                type="radio"
+                name={"radio-input-" + radioId}
+                value={option.value}
+                checked={option.value === selectedValue}
+                onChange={() => {
+                  setSelectedValue(option.value);
+                }}
+              />
+              <label
+                className="nhsuk-label nhsuk-radios__label"
+                htmlFor={optionId}
+              >
+                {option.displayValue}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </fieldset>
+  );
+};
