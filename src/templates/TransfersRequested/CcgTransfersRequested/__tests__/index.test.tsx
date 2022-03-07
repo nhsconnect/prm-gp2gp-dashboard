0a7f12d6c53af6625c690b09929238a97fb16983
Alina Balusescu <alina.balusescu1@nhs.net>
2022-03-07 14:32:08 +0000

    PRMT-2495 [AB] Use showContentsNavigation toggle on transfersRequested pages - to ensure a visually common journey when feature toggle is off

--- src/templates/TransfersRequested/CcgTransfersRequested/__tests__/index.test.tsx
+++ src/templates/TransfersRequested/CcgTransfersRequested/__tests__/index.test.tsx
@@ -8,14 +8,24 @@ import practiceMetricsMock from "../../../../../__mocks__/practiceMetricsMock.js
 import userEvent from "@testing-library/user-event";
 
 jest.mock("no-scroll");
+import { useFeatureToggles } from "../../../../library/hooks/useFeatureToggle";
+import { when } from "jest-when";
 
-describe("CCG Transfers Requested template", () => {
+jest.mock("../../../../library/hooks/useFeatureToggle");
+
+describe("CCG Transfers Requested template - showContentsNavigation feature toggle is on", () => {
   const pipelineCCGData = {
     odsCode: "12A",
     name: "BURTON CCG",
     ccgPractices: practiceMetricsMock,
     layout: "general",
   };
+
+  beforeEach(() => {
+    when(jest.mocked(useFeatureToggles))
+      .calledWith()
+      .mockReturnValue({ showContentsNavigation: true });
+  });
 
   it("displays only organisation ODS code when the name is not provided", () => {
     const odsCode = "Y00159";
@@ -216,5 +226,221 @@ describe("CCG Transfers Requested template", () => {
 
     expect(contentsHeader).toBeInTheDocument();
     expect(contentsLink).toBeInTheDocument();
+  });
+});
+
+describe("CCG Transfers Requested template - showContentsNavigation feature toggle is off", () => {
+  const pipelineCCGData = {
+    odsCode: "12A",
+    name: "BURTON CCG",
+    ccgPractices: practiceMetricsMock,
+    layout: "general",
+  };
+
+  beforeEach(() => {
+    when(jest.mocked(useFeatureToggles))
+      .calledWith()
+      .mockReturnValue({ showContentsNavigation: false });
+  });
+
+  it("displays only organisation ODS code when the name is not provided", () => {
+    const odsCode = "Y00159";
+    const ccgWithoutNameData = {
+      odsCode,
+      name: "",
+      ccgPractices: practiceMetricsMock,
+      layout: "general",
+    };
+
+    const { getByRole } = render(
+      <CcgTransfersRequested pageContext={ccgWithoutNameData} />
+    );
+
+    const expectedCcgHeading = getByRole("heading", {
+      name: /Y00159/,
+      level: 1,
+    });
+
+    expect(expectedCcgHeading).toBeInTheDocument();
+  });
+
+  it("renders CCG name and ODS code title correctly", () => {
+    const ccgHeadingText = /Burton CCG - 12A/;
+
+    const { getByRole } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const expectedCcgHeading = getByRole("heading", {
+      name: ccgHeadingText,
+      level: 1,
+    });
+
+    expect(expectedCcgHeading).toBeInTheDocument();
+  });
+
+  it("renders page title correctly", () => {
+    const { getByRole } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const pageTitle = getByRole("heading", {
+      name: "GP2GP transfers requested for registering practices",
+      level: 2,
+    });
+
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  it("renders table caption correctly", () => {
+    const { getByText } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const tableCaption = getByText(
+      "GP2GP transfers requested for registering practices - February 2020"
+    );
+
+    expect(tableCaption).toBeInTheDocument();
+  });
+
+  it("renders page description correctly", () => {
+    const { getByText } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const tableDescription = getByText(
+      "number of registrations that triggered a GP2GP transfer",
+      { exact: false }
+    );
+
+    expect(tableDescription).toBeInTheDocument();
+  });
+
+  it("displays modal with definitions when icon is clicked", async () => {
+    const {
+      findByText,
+      findAllByText,
+      queryByText,
+      queryAllByText,
+      getByRole,
+    } = render(<CcgTransfersRequested pageContext={pipelineCCGData} />);
+
+    expect(
+      queryByText(
+        /GP2GP transfers requested between the 1st and last day of the month that failed for a technical reason/
+      )
+    ).not.toBeInTheDocument();
+    expect(
+      queryAllByText(/and should be reported to the system supplier/)
+    ).toHaveLength(1);
+
+    const transfersReceivedHeader = getByRole("columnheader", {
+      name: /GP2GP technical failures/,
+    });
+
+    const transfersReceivedModalButton = within(
+      transfersReceivedHeader
+    ).getByRole("button");
+    userEvent.click(transfersReceivedModalButton);
+
+    expect(
+      await findByText(
+        /GP2GP transfers requested between the 1st and last day of the month that failed for a technical reason/
+      )
+    ).toBeInTheDocument();
+    expect(
+      await findAllByText(/and should be reported to the system supplier/)
+    ).toHaveLength(2);
+  });
+
+  it("displays help icons for all relevant headers", () => {
+    const { getAllByRole } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const allColumnHeaders = getAllByRole("columnheader");
+
+    const buttonOptions = { name: "Open modal with definition" };
+
+    expect(
+      within(allColumnHeaders[0]).queryByRole("button", buttonOptions)
+    ).not.toBeInTheDocument();
+    expect(
+      within(allColumnHeaders[1]).getByRole("button", buttonOptions)
+    ).toBeInTheDocument();
+    expect(
+      within(allColumnHeaders[2]).getByRole("button", buttonOptions)
+    ).toBeInTheDocument();
+    expect(
+      within(allColumnHeaders[3]).getByRole("button", buttonOptions)
+    ).toBeInTheDocument();
+  });
+
+  it("labels modal with content title", async () => {
+    const { getByRole, findByLabelText } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const transfersReceivedHeader = getByRole("columnheader", {
+      name: /GP2GP transfers received/,
+    });
+
+    const transfersReceivedHeaderButton = within(
+      transfersReceivedHeader
+    ).getByRole("button");
+    userEvent.click(transfersReceivedHeaderButton);
+
+    const within8DaysModal = await findByLabelText("GP2GP transfers received");
+    expect(within8DaysModal).toBeInTheDocument();
+  });
+
+  it("displays CCG practices and hides about and definitions content", () => {
+    const definitionsText =
+      "Percentage of GP2GP transfers between the 1st and last day of the month that were successfully received by the registering practice.";
+
+    const { getByText, queryByText, getAllByRole } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const allRows = getAllByRole("row");
+
+    expect(getByText("GP Practice - A12345")).toBeInTheDocument();
+    expect(getByText("Second GP Practice - A12346")).toBeInTheDocument();
+    expect(allRows[1]).toHaveTextContent(
+      /Registrations that triggered GP2GP transfer(.*)7/
+    );
+    expect(allRows[1]).toHaveTextContent(/GP2GP transfers received(.*)71.42%/);
+    expect(allRows[1]).toHaveTextContent(/GP2GP technical failures(.*)28.6%/);
+
+    expect(
+      queryByText("This site is updated 14 days after the end of each month.", {
+        exact: false,
+      })
+    ).not.toBeInTheDocument();
+
+    expect(
+      queryByText(definitionsText, {
+        exact: false,
+      })
+    ).not.toBeInTheDocument();
+  });
+
+  it("does not display contents navigation", async () => {
+    const { queryByRole } = render(
+      <CcgTransfersRequested pageContext={pipelineCCGData} />
+    );
+
+    const contentsHeader = queryByRole("heading", {
+      name: "Contents",
+      level: 2,
+    });
+
+    const contentsLink = queryByRole("link", {
+      name: "Integration times",
+    });
+
+    expect(contentsHeader).not.toBeInTheDocument();
+    expect(contentsLink).not.toBeInTheDocument();
   });
 });
--- src/templates/TransfersRequested/CcgTransfersRequested/index.tsx
+++ src/templates/TransfersRequested/CcgTransfersRequested/index.tsx
@@ -18,6 +18,7 @@ import {
 } from "../../../components/Definitions";
 import practiceTableContent from "../../../data/content/practiceTransfersRequestedSortOptions.json";
 import "../../index.scss";
+import { useFeatureToggles } from "../../../library/hooks/useFeatureToggle";
 
 type PageContext = {
   odsCode: string;
@@ -33,7 +34,7 @@ type CcgProps = {
 const CcgTransfersRequested: FC<CcgProps> = ({ pageContext }) => {
   const { name, odsCode, ccgPractices } = pageContext;
   const formattedName: string = convertToTitleCase(name);
-
+  const { showContentsNavigation } = useFeatureToggles();
   const { year, month } = ccgPractices[0].metrics[0];
   const tableTitle = `GP2GP transfers requested for registering practices - ${convertMonthNumberToText(
     month
@@ -77,95 +78,189 @@ const CcgTransfersRequested: FC<CcgProps> = ({ pageContext }) => {
         />
         <noscript>{`<style>.gp2gp-sort, .gp2gp-tabs, .gp2gp-open-modal-btn {display: none}</style>`}</noscript>
       </Helmet>
-      <div className="gp2gp-page-content-wrapper">
-        <h1 className="nhsuk-u-margin-bottom-5 gp2gp-page-heading">
-          {formattedName ? `${formattedName} - ${odsCode}` : odsCode}
-          <span className="nhsuk-u-visually-hidden">
-            {" "}
-            GP2GP transfers requested
-          </span>
-        </h1>
-        <div className="gp2gp-side-nav">
-          <ContentsList items={contentListItems} />
+      {showContentsNavigation ? (
+        <div className="gp2gp-page-content-wrapper">
+          <h1 className="nhsuk-u-margin-bottom-5 gp2gp-page-heading">
+            {formattedName ? `${formattedName} - ${odsCode}` : odsCode}
+            <span className="nhsuk-u-visually-hidden">
+              {" "}
+              GP2GP transfers requested
+            </span>
+          </h1>
+          <div className="gp2gp-side-nav">
+            <ContentsList items={contentListItems} />
+          </div>
+          <PageContent
+            className="gp2gp-page-contents"
+            title={pageTitle}
+            tableDescription={
+              <>
+                <p>The table below shows the</p>
+                <ul>
+                  <li>
+                    number of registrations that triggered a GP2GP transfer
+                  </li>
+                  <li>
+                    percentage of GP2GP transfers that were successfully
+                    received
+                  </li>
+                  <li>
+                    percentage of GP2GP transfers that failed for a technical
+                    reason
+                  </li>
+                </ul>
+              </>
+            }
+            expanderTitle="What happens when a GP2GP transfer fails?"
+            expanderContent={<WhatHappensWhenAGP2GPTransferFails />}
+            definitionsContent={<TransfersRequestedDefinitionsContent />}
+            tableContent={
+              <PracticeTableWithSort
+                ccgPractices={ccgPracticeTableData}
+                headers={[
+                  { title: "Registering practice name " },
+                  {
+                    title: "Registrations that triggered GP2GP transfer ",
+                    extra: (
+                      <HelpModal
+                        ariaLabelledBy="triggered-transfers-modal-title"
+                        iconHiddenDescription="Open modal with definition"
+                        content={
+                          <RegistrationsTriggeredByGP2GPDefinition ariaLabelId="triggered-transfers-modal-title" />
+                        }
+                      />
+                    ),
+                  },
+                  {
+                    title: "GP2GP transfers received ",
+                    extra: (
+                      <HelpModal
+                        ariaLabelledBy="transfers-received-modal-title"
+                        iconHiddenDescription="Open modal with definition"
+                        content={
+                          <TransfersReceivedPercentageDefinition ariaLabelId="transfers-received-modal-title" />
+                        }
+                      />
+                    ),
+                  },
+                  {
+                    title: (
+                      <>
+                        GP2GP technical failures{" "}
+                        <div className="gp2gp-title-emphasis">
+                          (paper copy requested){" "}
+                        </div>
+                      </>
+                    ),
+                    extra: (
+                      <HelpModal
+                        ariaLabelledBy="technical-failures-modal-title"
+                        iconHiddenDescription="Open modal with definition"
+                        content={
+                          <GP2GPTechnicalFailuresDefinition ariaLabelId="technical-failures-modal-title" />
+                        }
+                      />
+                    ),
+                  },
+                ]}
+                pageTemplatePath={PageTemplatePath.GP2GPTransfersRequested}
+                sortBySelect={practiceTableContent.sortBySelect}
+                orderSelect={practiceTableContent.orderSelect}
+                tableCaption={tableTitle}
+              />
+            }
+          />
         </div>
-        <PageContent
-          className="gp2gp-page-contents"
-          title={pageTitle}
-          tableDescription={
-            <>
-              <p>The table below shows the</p>
-              <ul>
-                <li>number of registrations that triggered a GP2GP transfer</li>
-                <li>
-                  percentage of GP2GP transfers that were successfully received
-                </li>
-                <li>
-                  percentage of GP2GP transfers that failed for a technical
-                  reason
-                </li>
-              </ul>
-            </>
-          }
-          expanderTitle="What happens when a GP2GP transfer fails?"
-          expanderContent={<WhatHappensWhenAGP2GPTransferFails />}
-          definitionsContent={<TransfersRequestedDefinitionsContent />}
-          tableContent={
-            <PracticeTableWithSort
-              ccgPractices={ccgPracticeTableData}
-              headers={[
-                { title: "Registering practice name " },
-                {
-                  title: "Registrations that triggered GP2GP transfer ",
-                  extra: (
-                    <HelpModal
-                      ariaLabelledBy="triggered-transfers-modal-title"
-                      iconHiddenDescription="Open modal with definition"
-                      content={
-                        <RegistrationsTriggeredByGP2GPDefinition ariaLabelId="triggered-transfers-modal-title" />
-                      }
-                    />
-                  ),
-                },
-                {
-                  title: "GP2GP transfers received ",
-                  extra: (
-                    <HelpModal
-                      ariaLabelledBy="transfers-received-modal-title"
-                      iconHiddenDescription="Open modal with definition"
-                      content={
-                        <TransfersReceivedPercentageDefinition ariaLabelId="transfers-received-modal-title" />
-                      }
-                    />
-                  ),
-                },
-                {
-                  title: (
-                    <>
-                      GP2GP technical failures{" "}
-                      <div className="gp2gp-title-emphasis">
-                        (paper copy requested){" "}
-                      </div>
-                    </>
-                  ),
-                  extra: (
-                    <HelpModal
-                      ariaLabelledBy="technical-failures-modal-title"
-                      iconHiddenDescription="Open modal with definition"
-                      content={
-                        <GP2GPTechnicalFailuresDefinition ariaLabelId="technical-failures-modal-title" />
-                      }
-                    />
-                  ),
-                },
-              ]}
-              pageTemplatePath={PageTemplatePath.GP2GPTransfersRequested}
-              sortBySelect={practiceTableContent.sortBySelect}
-              orderSelect={practiceTableContent.orderSelect}
-              tableCaption={tableTitle}
-            />
-          }
-        />
-      </div>
+      ) : (
+        <>
+          <h1 className="nhsuk-u-margin-bottom-5">
+            {formattedName ? `${formattedName} - ${odsCode}` : odsCode}
+            <span className="nhsuk-u-visually-hidden">
+              {" "}
+              GP2GP transfers requested
+            </span>
+          </h1>
+          <PageContent
+            title={pageTitle}
+            tableDescription={
+              <>
+                <p>The table below shows the</p>
+                <ul>
+                  <li>
+                    number of registrations that triggered a GP2GP transfer
+                  </li>
+                  <li>
+                    percentage of GP2GP transfers that were successfully
+                    received
+                  </li>
+                  <li>
+                    percentage of GP2GP transfers that failed for a technical
+                    reason
+                  </li>
+                </ul>
+              </>
+            }
+            expanderTitle="What happens when a GP2GP transfer fails?"
+            expanderContent={<WhatHappensWhenAGP2GPTransferFails />}
+            definitionsContent={<TransfersRequestedDefinitionsContent />}
+            tableContent={
+              <PracticeTableWithSort
+                ccgPractices={ccgPracticeTableData}
+                headers={[
+                  { title: "Registering practice name " },
+                  {
+                    title: "Registrations that triggered GP2GP transfer ",
+                    extra: (
+                      <HelpModal
+                        ariaLabelledBy="triggered-transfers-modal-title"
+                        iconHiddenDescription="Open modal with definition"
+                        content={
+                          <RegistrationsTriggeredByGP2GPDefinition ariaLabelId="triggered-transfers-modal-title" />
+                        }
+                      />
+                    ),
+                  },
+                  {
+                    title: "GP2GP transfers received ",
+                    extra: (
+                      <HelpModal
+                        ariaLabelledBy="transfers-received-modal-title"
+                        iconHiddenDescription="Open modal with definition"
+                        content={
+                          <TransfersReceivedPercentageDefinition ariaLabelId="transfers-received-modal-title" />
+                        }
+                      />
+                    ),
+                  },
+                  {
+                    title: (
+                      <>
+                        GP2GP technical failures{" "}
+                        <div className="gp2gp-title-emphasis">
+                          (paper copy requested){" "}
+                        </div>
+                      </>
+                    ),
+                    extra: (
+                      <HelpModal
+                        ariaLabelledBy="technical-failures-modal-title"
+                        iconHiddenDescription="Open modal with definition"
+                        content={
+                          <GP2GPTechnicalFailuresDefinition ariaLabelId="technical-failures-modal-title" />
+                        }
+                      />
+                    ),
+                  },
+                ]}
+                pageTemplatePath={PageTemplatePath.GP2GPTransfersRequested}
+                sortBySelect={practiceTableContent.sortBySelect}
+                orderSelect={practiceTableContent.orderSelect}
+                tableCaption={tableTitle}
+              />
+            }
+          />
+        </>
+      )}
     </>
   );
 };
