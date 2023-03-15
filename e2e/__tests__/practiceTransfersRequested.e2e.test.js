e081bf50af68d14f1881d23a011f7c281f6a32fe
Jake Newman <jacob.newman1@nhs.net>
2023-03-15 10:30:38 +0000

    PRMT-3117 [JN/JS] Bump cypress version to try to fix flakey e2e tests and force select box selection

--- e2e/__tests__/practiceTransfersRequested.e2e.test.js
+++ e2e/__tests__/practiceTransfersRequested.e2e.test.js
@@ -126,7 +126,7 @@ describe("Practice transfers requested page", () => {
 
         cy.get('[data-testid="table__cell--row-0-col-2"]').contains("66.7%");
 
-        cy.get("select#unitsSelect").select("Numbers");
+        cy.get("select#unitsSelect").select("Numbers", { force: true });
         cy.get("select#unitsSelect option:selected").should(
           "have.text",
           "Numbers"
