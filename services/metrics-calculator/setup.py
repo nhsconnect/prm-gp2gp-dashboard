1adaaded881f42e73f59fbeec03c57355b1c29b0
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-08-27 16:48:38 +0100

    [PRM-487] Setting pyArrow to version 20.0.0 explicitly (#61)

--- services/metrics-calculator/setup.py
+++ services/metrics-calculator/setup.py
@@ -5,5 +5,5 @@ setup(
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=["python-dateutil>=2.8", "boto3>=1.18", "urllib3==1.26.18", "PyArrow>=5.0"],
+    install_requires=["python-dateutil>=2.8", "boto3>=1.18", "urllib3==1.26.18", "PyArrow==20.0.0"],
 )
